6596db76f938324b62313dde348f7bc986e620bc
kuangc <kuangc@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-06-13 15:55:02 +0000

    remove duplicate external lib references

--- SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
+++ SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
@@ -3,18 +3,27 @@ package org.mitre.schemastore.client;
 import org.mitre.schemastore.model.Schema;
 import org.mitre.schemastore.model.SchemaElement;
 
+import java.util.ArrayList;
+
 public class SchemaStoreTest
 {
 	static public void main(String args[])
 	{
-		SchemaStoreClient client = new SchemaStoreClient("http://brainsrv2:8090/SchemaStore/services/SchemaStore");
+		SchemaStoreClient client = new SchemaStoreClient("http://localhost:8080/SchemaStore/services/SchemaStore");
 
 		// Display remaining schemas
 		try {
-			for(Schema schema : client.getSchemas())
-				System.out.println(schema.getId() + " " + schema.getName());
+		  Schema s = client.getSchema(new Integer(10));
+		  System.out.println(s.toString());
+		  
+		  ArrayList<Schema> schemas = client.getSchemas();
+          System.out.println("getSchemas() did not choke; # schemas: "+schemas.size());
+		  for(Schema schema : schemas)
+              System.out.println(schema.getId() + " " + schema.getName());
 			for(SchemaElement schemaElement : client.getSchemaElements(0))
 				System.out.println(schemaElement.getId() + " " + schemaElement.getName());
-		} catch(Exception e) {}
+		} catch(Exception e) {
+		  e.printStackTrace();
+		}
 	}
 }
