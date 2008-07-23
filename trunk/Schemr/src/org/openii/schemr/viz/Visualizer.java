cbb8c5f0164b5dfc9940681edd17db4c42b78bed
kuangc <kuangc@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-07-23 03:44:47 +0000

    ranked and selectable schema search results

--- trunk/Schemr/src/org/openii/schemr/viz/Visualizer.java
+++ trunk/Schemr/src/org/openii/schemr/viz/Visualizer.java
@@ -1,18 +1,23 @@
 package org.openii.schemr.viz;
 
+import java.awt.Component;
+
 public interface Visualizer {
 
 	public static final String NAME = "name";
 	public static final String DESC = "description";
 	public static final String TYPE = "type";
 	public static final String ID = "id";
+	public static final String MATCHED = "matched";
+	public static final String SCORE = "score";
+	public static final String MATCHED_OBJ = "matchedObject";
 
 	public static final String SCHEMA = "schema";
 	public static final String ENTITY = "entity";
 	public static final String ATTRIBUTE = "attribute";
 	public static final String CONTAINMENT = "containment";
 	public static final String RELATIONSHIP = "relationship";
-	
-	void show();
 
+	public Component getDisplayComponent();
+	
 }
