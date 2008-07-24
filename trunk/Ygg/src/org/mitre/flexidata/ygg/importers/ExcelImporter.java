3542b1ed41c157ed9df06160b28cc41415aa2709
haoli@mitre.org <haoli@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-07-24 22:02:08 +0000

    add Excel Importer

---
+++ trunk/Ygg/src/org/mitre/flexidata/ygg/importers/ExcelImporter.java
@@ -0,0 +1,168 @@
+package org.mitre.flexidata.ygg.importers;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import org.mitre.schemastore.model.Attribute;
+import org.mitre.schemastore.model.Domain;
+import org.mitre.schemastore.model.Entity;
+import org.mitre.schemastore.model.SchemaElement;
+
+/**
+ * 
+ * ExcelConverter is a poor man's importer. It imports a relational definition
+ * stored in an excel sheet The format of the schema is [table name][column
+ * name] [documentation] A row with only table name (with no column name)
+ * contains name and documentation for the table.
+ * 
+ * @author HAOLI
+ * 
+ */
+
+public class ExcelImporter extends Importer {
+	private Workbook _excelWorkbook;
+	private URI _excelURI;
+	private HashMap<String, Entity> _entities = new HashMap<String, Entity>();
+	private HashMap<String, Attribute> _attributes = new HashMap<String, Attribute>();
+	private HashMap<String, Domain> _domains = new HashMap<String, Domain>();
+
+	// get rid of characters
+	protected String despace(String s) {
+		return s.trim().replaceAll(" ", "_");
+	}
+
+	protected void generate() {
+		Sheet[] sheets = _excelWorkbook.getSheets();
+
+		// iterate and load individual work sheets
+		for (int s = 0; s < sheets.length; s++) {
+			Sheet sheet = sheets[s];
+			int numRows = sheet.getRows();
+
+			// iterate through rows and create table/attribute nodes
+			for (int i = 0; i < numRows; i++) {
+				readRow(sheet.getRow(i));
+			}
+		}
+	}
+
+	protected void readRow(Cell[] cells) {
+		String tblName = despace(cells[0].getContents());
+		String attName = despace(cells[1].getContents());
+		String documentation = cells[2].getContents().trim();
+		Entity tblEntity;
+		Attribute attribute;
+
+		if (tblName.length() == 0 && attName.length() == 0)
+			return;
+
+		// create an entity for a table
+		if (!_entities.containsKey(tblName)) {
+			tblEntity = new Entity(nextId(), tblName, "", 0);
+			_entities.put(tblName, tblEntity);
+		} else
+			tblEntity = _entities.get(tblName);
+
+		// create an attribute
+		if (attName.length() > 0) {
+			Integer domainID = _domains.get(ANY).getId();
+			attribute = new Attribute(nextId(), attName, documentation, tblEntity.getId(),
+					domainID, null, null, 0);
+			_attributes.put(attribute.getName(), attribute);
+		} else {
+			// doc describes table if attribute doesn't exist
+			tblEntity.setDescription(documentation);
+		}
+	}
+
+	private void loadDomains() {
+		Domain anyDomain = new Domain(nextId(), ANY, "unknown domain", 0);
+		_domains.put(anyDomain.getName(), anyDomain);
+	}
+
+	public ArrayList<String> getFileTypes() {
+		ArrayList<String> filetypes = new ArrayList<String>(2);
+		filetypes.add("xls");
+		filetypes.add("csv");
+		return filetypes;
+	}
+
+	private void initialize(URI uri) throws IOException, URISyntaxException, BiffException {
+		InputStream excelStream;
+		// Do nothing if the excel sheet has been cached.
+		if (_excelURI != null && _excelURI.equals(uri))
+			return;
+
+		_excelURI = uri;
+		excelStream = _excelURI.toURL().openStream();
+		_excelWorkbook = Workbook.getWorkbook(excelStream);
+		excelStream.close();
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		File excel = new File("Example.xls");
+		ExcelImporter tester = new ExcelImporter();
+		try {
+			tester.importSchema(excel.getName(), "", "", excel.toURI().toString());
+		} catch (ImporterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String getDescription() {
+		return "Imports Excel formatted schema into the schema store.";
+	}
+
+	@Override
+	public String getName() {
+		return "Excel Importer";
+	}
+
+	protected ArrayList<SchemaElement> generateSchemaElementList() {
+		ArrayList<SchemaElement> schemaElements = new ArrayList<SchemaElement>();
+
+		for (Entity e : _entities.values())
+			schemaElements.add(e);
+		for (Attribute a : _attributes.values())
+			schemaElements.add(a);
+		return schemaElements;
+	}
+
+	@Override
+	// TODO why schemaID in the parameter list?
+	protected ArrayList<SchemaElement> getSchemaElements(Integer schemaID, String uri)
+			throws ImporterException {
+		URI importUri;
+		try {
+			importUri = new URI(uri);
+			initialize(importUri);
+			loadDomains();
+			generate();
+		} catch (URISyntaxException e) {
+			throw new ImporterException (ImporterException.INVALID_URI, e.getMessage());
+		} catch (IOException e) {
+			throw new ImporterException( ImporterException.PARSE_FAILURE, e.getMessage());
+		} catch (BiffException b){
+			throw new ImporterException(ImporterException.IMPORT_FAILURE, b.getMessage());
+		}
+		return generateSchemaElementList();
+	}
+
+	@Override
+	public Integer getURIType() {
+		return FILE;
+	}
+
+}
