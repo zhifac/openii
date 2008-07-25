26c51c98ccb4b9933ea2447768e2d93fbfa18f23
haoli@mitre.org <haoli@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-07-25 22:17:06 +0000

    fixed URI problem in excel and domain value importer

--- trunk/Ygg/src/org/mitre/flexidata/ygg/importers/DomainValueImporter.java
+++ trunk/Ygg/src/org/mitre/flexidata/ygg/importers/DomainValueImporter.java
@@ -10,17 +10,17 @@ import org.mitre.schemastore.model.DomainValue;
 import org.mitre.schemastore.model.SchemaElement;
 
 /**
- * Imports domain values from an Excel sheet. The excel sheet format is 
- * [domain name] [domain value] [documentation] 
+ * Imports domain values from an Excel sheet. The excel sheet format is [domain
+ * name] [domain value] [documentation]
  * 
  * @author HAOLI
- *
+ * 
  */
 
 public class DomainValueImporter extends ExcelImporter {
 	HashMap<String, Domain> _domains = new HashMap<String, Domain>();
 	HashMap<String, DomainValue> _domainValues = new HashMap<String, DomainValue>();
-	
+
 	@Override
 	public String getDescription() {
 		return "Imports Excel formatted domain and domain values.";
@@ -28,9 +28,9 @@ public class DomainValueImporter extends ExcelImporter {
 
 	@Override
 	public String getName() {
-		return "Domain Value Excel Importer";
+		return "Domain Value Importer (xls)";
 	}
-	
+
 	protected ArrayList<SchemaElement> generateSchemaElementList() {
 		ArrayList<SchemaElement> schemaElements = new ArrayList<SchemaElement>();
 
@@ -40,7 +40,6 @@ public class DomainValueImporter extends ExcelImporter {
 			schemaElements.add(v);
 		return schemaElements;
 	}
-	
 
 	protected void readRow(Cell[] cells) {
 		String domainName = despace(cells[0].getContents());
@@ -53,21 +52,24 @@ public class DomainValueImporter extends ExcelImporter {
 			return;
 
 		// create an entity for a table
-		if (!_domains.containsKey(domainName)) {
-			domain = new Domain(nextId(), domainName, "", 0);
-			_domains.put(domainName, domain);
-		} else
+		if (_domains.containsKey(domainName))
 			domain = _domains.get(domainName);
+		else {
+			domain = new Domain(nextId(), domainName, "", 0);
+			System.err.println ("adding new domain " + domainName);
+			_domains.put(domainName, domain);
+		}
 
 		// create a domain value
 		if (domainValueStr.length() > 0) {
-			domainValue = new DomainValue(nextId(), domainValueStr, documentation, domain.getId(), 0);
-			_domainValues.put(domainValue.getName(), domainValue);
+			domainValue = new DomainValue(nextId(), domainValueStr, documentation, domain.getId(),
+					0);
+			System.err.println ("adding new domain value " + domain.getName() + " : " + domainValueStr);
+			_domainValues.put(domainValueStr, domainValue);
 		} else {
 			// doc describes domain if domain value is absent
 			domain.setDescription(documentation);
 		}
 	}
-
 
 }
