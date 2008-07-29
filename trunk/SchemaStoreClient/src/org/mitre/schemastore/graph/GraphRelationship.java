de072565241e9a89eae13f42a4acea031f74a6b8
dburdick@mitre.org <dburdick@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-07-29 22:34:36 +0000

    implemented getParent and getChild for the edge classes (Containment, Subtype, Relationship)

--- trunk/SchemaStoreClient/src/org/mitre/schemastore/graph/GraphRelationship.java
+++ trunk/SchemaStoreClient/src/org/mitre/schemastore/graph/GraphRelationship.java
@@ -15,18 +15,38 @@ import org.mitre.schemastore.model.SchemaElement;
 
 public class GraphRelationship extends Relationship implements GraphSchemaElement{
 
+	private GraphEntity parent;
+	private GraphEntity child;
+	
 	/** Constructs the containment relationship */
 	public GraphRelationship(Integer id, String name, Integer leftID, Integer leftMin, Integer leftMax, Integer rightID, Integer rightMin, Integer rightMax, Integer base)	
 		{ super(id, name, leftID, leftMin, leftMax, rightID, rightMin, rightMax, base); }
 	
 	public GraphRelationship(Relationship r)	
 		{ super(r.getId(), r.getName(), r.getLeftID(), r.getLeftMin(), r.getLeftMax(), r.getRightID(), r.getRightMin(), r.getRightMax(), r.getBase()); }
-
+	
+	public GraphEntity getParent(){
+		return parent;
+	}
+	public GraphEntity getChild(){
+		return child;
+	}
+	public void setChild(GraphEntity passedChild){
+		child = passedChild;
+	}
+	public void setParent(GraphEntity passedParent){
+		parent = passedParent;
+	}
+	
 	public ArrayList<SchemaElement> getParents(){
-		return new ArrayList<SchemaElement>();
+		ArrayList<SchemaElement> retVal = new ArrayList<SchemaElement>();
+		retVal.add(parent);
+		return retVal;
 	}
 	
 	public ArrayList<SchemaElement> getChildren(){
-		return new ArrayList<SchemaElement>();
+		ArrayList<SchemaElement> retVal = new ArrayList<SchemaElement>();
+		retVal.add(child);
+		return retVal;
 	}
 }
