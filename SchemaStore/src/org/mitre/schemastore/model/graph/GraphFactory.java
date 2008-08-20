d82a999bb4b9abed1d6fd530e1e53ea180cbeccc
cwolf <cwolf@mitre.org>
2008-08-20 14:19:11 +0000

    Move graph structure to SchemaStore so it can be included in model

---
+++ SchemaStore/src/org/mitre/schemastore/model/graph/GraphFactory.java
@@ -0,0 +1,37 @@
+// Copyright 2008 The MITRE Corporation. ALL RIGHTS RESERVED.
+
+package org.mitre.schemastore.model.graph;
+
+import java.util.ArrayList;
+
+import org.mitre.schemastore.model.Containment;
+import org.mitre.schemastore.model.Domain;
+import org.mitre.schemastore.model.DomainValue;
+import org.mitre.schemastore.model.SchemaElement;
+
+/**
+ *  Class used to generate a hierarchical graph
+ */
+public class GraphFactory
+{
+	/** Generates a hierarchical graph */
+	static public HierarchicalGraph generateGraph(Integer schemaID, ArrayList<SchemaElement> elements)
+	{
+		// Build the graph
+		Graph graph = new Graph(schemaID, elements);
+
+		// Determine the makeup of the schema
+		Integer totalCount=0, domainCount=0, containmentCount=0;
+		for(SchemaElement element : graph.getSchemaElements())
+		{
+			if(element instanceof Domain || element instanceof DomainValue) domainCount++;
+			if(element instanceof Containment) containmentCount++;
+			totalCount++;
+		}
+		
+		// Identify which hierarchical graph to use
+		if(domainCount==totalCount) return new DomainGraph(graph);
+		if(containmentCount>0) return new ContainmentGraph(graph);
+		return new RelationalGraph(graph);
+	}
+}
