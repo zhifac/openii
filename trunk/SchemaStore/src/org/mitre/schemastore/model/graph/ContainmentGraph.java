fd92abff2b4446d11ac0012929fc90fa121b4715
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-08-20 14:19:11 +0000

    Move graph structure to SchemaStore so it can be included in model

---
+++ trunk/SchemaStore/src/org/mitre/schemastore/model/graph/ContainmentGraph.java
@@ -0,0 +1,81 @@
+// Copyright 2008 The MITRE Corporation. ALL RIGHTS RESERVED.
+
+package org.mitre.schemastore.model.graph;
+
+import java.util.ArrayList;
+
+import org.mitre.schemastore.model.Containment;
+import org.mitre.schemastore.model.Domain;
+import org.mitre.schemastore.model.SchemaElement;
+
+/**
+ *  Class for displaying containment hierarchy
+ */
+public class ContainmentGraph extends HierarchicalGraph
+{
+	/** Constructs the containment graph */
+	public ContainmentGraph(Graph graph)
+		{ super(graph); }
+
+	/** Returns the root elements in this graph */
+	public ArrayList<SchemaElement> getRootElements()
+	{
+		ArrayList<SchemaElement> rootElements = new ArrayList<SchemaElement>();
+
+		// Find all containments whose root is null
+		for(SchemaElement element : getSchemaElements(Containment.class))
+			if(getSchemaElement(((Containment)element).getParentID())==null)
+				rootElements.add(element);
+			
+		return rootElements;
+	}
+	
+	/** Returns the parent elements of the specified element in this graph */
+	public ArrayList<SchemaElement> getParentElements(SchemaElement element)
+	{
+		ArrayList<SchemaElement> parentElements = new ArrayList<SchemaElement>();
+		
+		// Find all containments one level higher up the graph
+		if(element instanceof Containment)
+		{
+			Integer elementID = ((Containment)element).getParentID();
+			if(elementID!=null)
+				for(Containment containment : getContainments(elementID))
+					if(containment.getChildID().equals(elementID))
+						parentElements.add(containment);
+		}
+			
+		return parentElements;
+	}
+	
+	/** Returns the children elements of the specified element in this graph */
+	public ArrayList<SchemaElement> getChildElements(SchemaElement element)
+	{
+		ArrayList<SchemaElement> childElements = new ArrayList<SchemaElement>();
+		
+		// Find all containments one level lower on the graph
+		if(element instanceof Containment)
+		{
+			Integer elementID = ((Containment)element).getChildID();
+			for(Containment containment : getContainments(elementID))
+				if(containment.getParentID().equals(elementID))
+					childElements.add(containment);
+		}
+			
+		return childElements;
+	}
+
+	/** Returns the domains of the specified element in this graph */
+	public Domain getDomain(SchemaElement element)
+	{
+		// Find the domain attached to this containment
+		if(element instanceof Containment)
+		{
+			Integer elementID = ((Containment)element).getChildID();
+			SchemaElement childElement = getSchemaElement(elementID);
+			if(childElement instanceof Domain)
+				return (Domain)childElement;
+		}			
+		return null;	
+	}
+}
