9b8200a58e1ca3d543c165cb6e2233820ba556a4
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-09-24 18:44:14 +0000

    Fix problem in graph constructor

--- trunk/SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
+++ trunk/SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
@@ -1,41 +1,17 @@
 package org.mitre.schemastore.client;
 
 import org.mitre.schemastore.model.Schema;
-import org.mitre.schemastore.model.graph.HierarchicalGraph;
 
 public class SchemaStoreTest
 {
 	static public void main(String args[])
 	{
-		SchemaStoreClient client = new SchemaStoreClient("http://brainsrv2:8090/SchemaStore/services/SchemaStore");
+		SchemaStoreClient client = new SchemaStoreClient("http://localhost:8080/SchemaStore2/services/SchemaStore");
 
 		// Display remaining schemas
-//		try {
-//			for(Schema schema : client.getSchemas())
-//				System.out.println(schema.getId() + " " + schema.getName());
-//			for(SchemaElement schemaElement : client.getSchemaElements(0))
-//				System.out.println(schemaElement.getId() + " " + schemaElement.getName());
-//		} catch(Exception e) { System.out.println(e.getMessage());}
-
-		//test getSynonyms
 		try {
-			for (String synset: client.getSynonyms(52527))
-				{
-				System.out.println(synset);
-			}
-		} catch(Exception e) { System.out.println(e.getMessage());}
-
-//		//test addWordsAndSynonymsForAllSchemas
-//		try {
-//			client.addWordsAndSynonymsForAllSchemas()
-//			}
-//		} catch(Exception e) { System.out.println(e.getMessage());}
-//		
 			for(Schema schema : client.getSchemas())
-				System.out.println(schema.getId() + " " + schema.getName());
-			
-			HierarchicalGraph graph = client.getSchemaElementGraph(107691);
-			System.out.println(graph.toString());
+				System.out.println(schema.getId() + ": " + schema.getName());
 		}
 		catch(Exception e) { e.printStackTrace(); }
 	}
