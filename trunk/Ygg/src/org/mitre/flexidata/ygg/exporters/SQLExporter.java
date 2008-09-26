e7a29156edd47015bb5bafbd028662d01bb2c9a6
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-09-26 15:24:05 +0000

    Change reference to Graph from HierarchicalGraph

--- trunk/Ygg/src/org/mitre/flexidata/ygg/exporters/SQLExporter.java
+++ trunk/Ygg/src/org/mitre/flexidata/ygg/exporters/SQLExporter.java
@@ -388,7 +388,7 @@ public class SQLExporter extends Exporter {
 		} else {
 			SQLExporter exporter = new SQLExporter();
 			try {
-				System.out.println( exporter.exportSchema(new Integer(schemaId), ss.getSchemaElementGraph(schemaId).getElements(null)) );
+				System.out.println( exporter.exportSchema(new Integer(schemaId), ss.getGraph(schemaId).getElements(null)) );
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
