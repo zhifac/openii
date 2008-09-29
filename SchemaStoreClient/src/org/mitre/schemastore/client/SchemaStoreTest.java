864bf06caa53eb3a93dda8baffc57da8fc8c7a76
cwolf <cwolf@mitre.org>
2008-09-29 19:17:21 +0000

    Fix Test script to reference SchemaStore versus SchemaStore2

--- SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
+++ SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
@@ -6,12 +6,16 @@ public class SchemaStoreTest
 {
 	static public void main(String args[])
 	{
-		SchemaStoreClient client = new SchemaStoreClient("http://localhost:8080/SchemaStore2/services/SchemaStore");
+		SchemaStoreClient client = new SchemaStoreClient("http://brainsrv2:8090/SchemaStore/services/SchemaStore");
 
 		// Display remaining schemas
 		try {
 			for(Schema schema : client.getSchemas())
 				System.out.println(schema.getId() + ": " + schema.getName());
+
+//			Graph graph = client.getGraph(3032);
+//			for(SchemaElement element : graph.getElements(null))
+//				System.out.println(element.getName());
 		}
 		catch(Exception e) { e.printStackTrace(); }
 	}
