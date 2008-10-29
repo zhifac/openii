6b81555fbcffa6f44b41652e3656d7727f1dc3db
cwolf <cwolf@mitre.org>
2008-10-29 22:05:25 +0000

    Modify Galaxy to use newest version of SchemaStore

--- Galaxy/WebContent/GalaxyApplet/model/server/ServletConnection.java
+++ Galaxy/WebContent/GalaxyApplet/model/server/ServletConnection.java
@@ -147,9 +147,9 @@ public class ServletConnection
 	static public SchemaElement getSchemaElement(Integer schemaElementID)
 		{ return (SchemaElement)callFunction("getSchemaElement",new Object[] {schemaElementID}); }	
 
-	/** Retrieves the base schema elements for the specified schema from the web service */ @SuppressWarnings("unchecked")
-	static public ArrayList<SchemaElement> getBaseSchemaElements(Integer schemaID)
-		{ return (ArrayList<SchemaElement>)callFunction("getBaseSchemaElements",new Object[] {schemaID}); }
+	/** Retrieves the schema elements which contain the specified keyword from the web service */ @SuppressWarnings("unchecked")
+	static public ArrayList<SchemaElement> getSchemaElementsWithKeyword(String keyword)
+		{ return (ArrayList<SchemaElement>)callFunction("getSchemaElementsWithKeyword",new Object[] {keyword}); }
 
 	//-----------------------
 	// Data Source Functions
