dca296019190c56577074b538040f4a4bedd8d02
cwolf <cwolf@mitre.org>
2008-12-01 20:18:54 +0000

    Fix accidentally checked in files

--- SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
+++ SchemaStoreClient/src/org/mitre/schemastore/client/SchemaStoreTest.java
@@ -8,24 +8,9 @@ public class SchemaStoreTest
 	{
 		// Display the schemas found within the repository
 		try {
-			SchemaStoreClient client = new SchemaStoreClient("C:\\chris\\projects\\Flexidata\\SchemaStore\\SchemaStore.jar");
-			
+			SchemaStoreClient client = new SchemaStoreClient("http://brainsrv2:8090/SchemaStore/services/SchemaStore");
 			for(Schema schema : client.getSchemas())
 				System.out.println(schema.getId() + ": " + schema.getName());
-
-			for(Integer schemaID : client.getDeletableSchemas())
-				System.out.println(schemaID);
-
-//			for(SchemaElement element : client.getGraph(52525).getElements(null))
-//				if(element.getDescription()!=null && !element.getDescription().equals(""))
-//					System.out.println(element.getId() + ": " + element.getName());
-			
-//			FilteredGraph graph = new FilteredGraph(new HierarchicalGraph(client.getGraph(177492)));
-//			graph.setFilteredRoot(177716);
-//			graph.setMinDepth(3);
-//			graph.setMaxDepth(4);
-//			for(SchemaElement element : graph.getFilteredElements())
-//				System.out.println(element.getName());
 		}
 		catch(Exception e) { e.printStackTrace(); }
 	}
