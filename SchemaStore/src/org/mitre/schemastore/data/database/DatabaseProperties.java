b99af699d260021946a5dd4e8f8c9edf8c3e4d75
jchoyt <jchoyt@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-12-15 19:52:30 +0000

    bad character for copyright symbol

--- SchemaStore/src/org/mitre/schemastore/data/database/DatabaseProperties.java
+++ SchemaStore/src/org/mitre/schemastore/data/database/DatabaseProperties.java
@@ -1,4 +1,4 @@
-// © The MITRE Corporation 2006
+// (c) The MITRE Corporation 2006
 // ALL RIGHTS RESERVED
 package org.mitre.schemastore.data.database;
 
@@ -25,30 +25,30 @@ public class DatabaseProperties
 	static private String databaseServer = null;
 	static private String databaseName = null;
 	static private String databaseUser = null;
-	static private String databasePassword = null;	
-	
+	static private String databasePassword = null;
+
 	/** Extracts database properties */
 	static
 	{
 		// Load database properties from file
-		try {			
+		try {
 			// Pull the entire file into a string
 			InputStream configStream = DatabaseProperties.class.getResourceAsStream("/schemastore.xml");
 			BufferedReader in = new BufferedReader(new InputStreamReader(configStream));
 			StringBuffer buffer = new StringBuffer("");
 			String line; while((line=in.readLine())!=null) buffer.append(line);
 			in.close();
-			
+
 			// Parse out the database server
 			Matcher serverMatcher = serverPattern.matcher(buffer);
 			if(serverMatcher.find())
 				databaseServer = serverMatcher.group(1);
-			
+
 			// Parse out the database name
 			Matcher nameMatcher = namePattern.matcher(buffer);
 			if(nameMatcher.find())
 				databaseName = nameMatcher.group(1);
-			
+
 			// Parse out the database user
 			Matcher userMatcher = userPattern.matcher(buffer);
 			if(userMatcher.find())
@@ -62,19 +62,19 @@ public class DatabaseProperties
 		catch(IOException e)
 			{ System.out.println("(E)DatabaseProperties - schemastore.xml has failed to load!\n"+e.getMessage()); }
 	}
-	
+
 	/** Returns the database server */
 	static public String getServer()
 		{ return databaseServer; }
-	
+
 	/** Returns the database name */
 	static public String getName()
 		{ return databaseName; }
-	
+
 	/** Returns the database user */
 	static public String getUser()
 		{ return databaseUser; }
-	
+
 	/** Returns the database password */
 	static public String getPassword()
 		{ return databasePassword; }
