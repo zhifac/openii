5716b07a0408080ef76bf4afc0487a7d4a872c7e
jchoyt <jchoyt@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2008-12-22 18:29:17 +0000

    added license blurbs, minor edits to facilitate integration with Eclipse

--- SchemaStoreClient/src/org/mitre/schemastore/importers/ImporterManager.java
+++ SchemaStoreClient/src/org/mitre/schemastore/importers/ImporterManager.java
@@ -1,4 +1,18 @@
-//Copyright 2008 The MITRE Corporation. ALL RIGHTS RESERVED.
+/*
+ *  Copyright 2008 The MITRE Corporation (http://www.mitre.org/). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.mitre.schemastore.importers;
 
@@ -29,6 +43,7 @@ public class ImporterManager
 		try {			
 			// Pull the entire file into a string
 			InputStream configStream = ImporterManager.class.getResourceAsStream("/importers.xml");
+			System.out.println("configStream " + String.valueOf(configStream));
 			BufferedReader in = new BufferedReader(new InputStreamReader(configStream));
 			StringBuffer buffer = new StringBuffer("");
 			String line; while((line=in.readLine())!=null) buffer.append(line);
@@ -70,6 +85,15 @@ public class ImporterManager
 	{
 		for(Importer importer : importers)
 			if(importer.getClass().equals(type))
+				return importer;
+		return null;
+	}
+	
+	/** Returns the specified importer - the argument must match the self-defined name of the Importer exactly */
+	public Importer getImporter(String type)
+	{
+		for(Importer importer : importers)
+			if(importer.getName().equals(type))
 				return importer;
 		return null;
 	}
