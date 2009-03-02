e9c5098598fb5966039e27745c17bf07b6ed7c57
cbonaceto@mitre.org <cbonaceto@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-03-02 15:19:27 +0000

    Fixed build path error (duplicate unity.jar entries).

--- trunk/OpenII/src/org/mitre/openii/views/UnityView.java
+++ trunk/OpenII/src/org/mitre/openii/views/UnityView.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.part.ViewPart;
