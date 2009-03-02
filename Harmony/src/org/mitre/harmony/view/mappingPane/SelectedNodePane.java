d0c88c4d25de1ad84812cd0041aa723127bde55d
cwolf <cwolf@mitre.org>
2009-03-02 13:50:04 +0000

    Add in Maya's changes to Harmony

--- Harmony/src/org/mitre/harmony/view/mappingPane/SelectedNodePane.java
+++ Harmony/src/org/mitre/harmony/view/mappingPane/SelectedNodePane.java
@@ -29,8 +29,8 @@ import org.mitre.schemastore.model.graph.HierarchicalGraph;
 class SelectedNodePane extends JPanel implements SelectedInfoListener
 {
 	/** Defines the color for the blue background */
-	static private final Color BLUE = new Color(100, 100, 255);
-
+	static private final Color BLUE = new Color(100,100,255);
+	
 	/** Tracks the role associated with this pane */
 	private Integer role = null;
 	
@@ -98,9 +98,8 @@ class SelectedNodePane extends JPanel implements SelectedInfoListener
 		if(displayName.length()>2) displayName = displayName.substring(0, displayName.length()-2);
 		
 		// Set the title and text panes
-		//		titlePane.setText("Information on '" + displayName + "'");
 		titlePane.setText(displayName);
-		textPane.setText("<html>" + getText(element, displayName) + "</html>");
+		textPane.setText("<html>"+getText(element,displayName)+"</html>");
 	}
 
 	/** Generates the text to display in the text pane */
@@ -109,7 +108,6 @@ class SelectedNodePane extends JPanel implements SelectedInfoListener
  		StringBuffer text = new StringBuffer();		
 		
 		// Display schema element name and description
-		//		text.append("<b>Name</b>: " + displayName + "<br>");
 		text.append("<b>Description</b>: " + element.getDescription() + "<br>");
 
 		// Collect domain information
