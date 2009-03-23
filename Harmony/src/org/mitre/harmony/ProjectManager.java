02566468580ff864b275c1294b300f34d362cdd0
cwolf <cwolf@mitre.org>
2009-03-23 16:44:33 +0000

    Fix project manager to be more robust to changes

--- Harmony/src/org/mitre/harmony/ProjectManager.java
+++ Harmony/src/org/mitre/harmony/ProjectManager.java
@@ -1,8 +1,10 @@
 package org.mitre.harmony;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
@@ -10,10 +12,7 @@ import java.util.ArrayList;
 import org.mitre.harmony.model.ConfigManager;
 import org.mitre.harmony.model.HarmonyConsts;
 import org.mitre.harmony.model.HarmonyModel;
-import org.mitre.harmony.model.mapping.MappingCellManager;
 import org.mitre.harmony.model.mapping.MappingListener;
-import org.mitre.harmony.model.mapping.MappingManager;
-import org.mitre.harmony.model.selectedInfo.SelectedInfoManager;
 import org.mitre.schemastore.model.Mapping;
 import org.mitre.schemastore.model.MappingCell;
 
@@ -41,97 +40,125 @@ public class ProjectManager implements MappingListener
 		}
 	}
 	
+	/** Loads the specified item */
+	private void load(ObjectInputStream in, String item)
+	{
+		try
+		{
+			// Loads the mapping
+			if(item.equals("Mapping"))
+				harmonyModel.getMappingManager().setMapping((Mapping)in.readObject());
+		
+			// Loads the mapping cells
+			if(item.equals("MappingCells"))
+			{
+				int mappingCellCount = in.readInt();
+				for(int i=0; i<mappingCellCount; i++)
+				{
+					MappingCell mappingCell = (MappingCell)in.readObject();
+					mappingCell.setId(null);
+					harmonyModel.getMappingCellManager().setMappingCell(mappingCell);
+				}
+			}
+			
+			// Loads if the mapping has been modified since saving to the repository			
+			if(item.equals("MappingModified"))
+				harmonyModel.getMappingManager().setModified(in.readBoolean());
+
+			// Loads the displayed schemas
+			if(item.equals("DisplayedSchemas"))
+			{
+				ArrayList<Integer> leftSchemas = new ArrayList<Integer>();
+				ArrayList<Integer> rightSchemas = new ArrayList<Integer>();
+				int leftSchemaCount = in.readInt();
+				for(int i=0; i<leftSchemaCount; i++) leftSchemas.add(in.readInt());
+				int rightSchemaCount = in.readInt();
+				for(int i=0; i<rightSchemaCount; i++) rightSchemas.add(in.readInt());
+				harmonyModel.getSelectedInfo().setSelectedSchemas(leftSchemas, rightSchemas);
+			}
+			
+		} catch(Exception e) {}
+	}
+	
+	/** Saves the specified item */
+	private void save(ObjectOutputStream out, String item)
+	{
+		try
+		{
+			// Save the item label
+			out.writeObject(item);
+		
+			// Saves the mapping
+			if(item.equals("Mapping"))
+				out.writeObject(harmonyModel.getMappingManager().getMapping());
+
+			// Saves the mapping cells
+			if(item.equals("MappingCells"))
+			{
+				ArrayList<MappingCell> mappingCells = harmonyModel.getMappingCellManager().getMappingCells();
+				out.writeInt(mappingCells.size());
+				for(MappingCell mappingCell : mappingCells) out.writeObject(mappingCell);
+			}
+
+			// Saves if the mapping has been modified since saving to the repository
+			if(item.equals("MappingModified"))
+				out.writeBoolean(harmonyModel.getMappingManager().isModified());
+
+			// Saves the displayed schemas
+			if(item.equals("DisplayedSchemas"))
+			{
+				ArrayList<Integer> leftSchemas = harmonyModel.getSelectedInfo().getSchemas(HarmonyConsts.LEFT);
+				out.writeInt(leftSchemas.size());
+				for(Integer leftSchema : leftSchemas) out.writeInt(leftSchema);
+				ArrayList<Integer> rightSchemas = harmonyModel.getSelectedInfo().getSchemas(HarmonyConsts.RIGHT);
+				out.writeInt(rightSchemas.size());
+				for(Integer rightSchema : rightSchemas) out.writeInt(rightSchema);
+			}
+			
+		} catch(IOException e) {}
+	}
+	
 	/** Instantiates the project manager to listen for any changes to the model */
 	public ProjectManager(HarmonyModel harmonyModel)
 	{
 		this.harmonyModel = harmonyModel;
-		
-		// Get the mapping model and mapping cell manager
-		MappingManager mappingManager = harmonyModel.getMappingManager();
-		MappingCellManager mappingCellManager = harmonyModel.getMappingCellManager();
-		
-		// Stores project information
-		String repository = "";
-		Mapping mapping = null;
-		ArrayList<MappingCell> mappingCells = new ArrayList<MappingCell>();
-		ArrayList<Integer> leftSchemas = new ArrayList<Integer>();
-		ArrayList<Integer> rightSchemas = new ArrayList<Integer>();
 		
 		// Get current project information from file
 		ObjectInputStream in = null;
 		try
 		{
 			in = new ObjectInputStream(new FileInputStream(projectFile));
-
-			// Retrieve repository, mapping, and if saved to repository
-			repository = (String)in.readObject();
-			mapping = (Mapping)in.readObject();
-			mappingManager.setModified(in.readBoolean());
-			
-			// Retrieve mapping cells
-			int mappingCellCount = in.readInt();
-			for(int i=0; i<mappingCellCount; i++)
-				mappingCells.add((MappingCell)in.readObject());
-
-			// Retrieves the displayed schemas
-			int leftSchemaCount = in.readInt();
-			for(int i=0; i<leftSchemaCount; i++) leftSchemas.add(in.readInt());
-			int rightSchemaCount = in.readInt();
-			for(int i=0; i<rightSchemaCount; i++) rightSchemas.add(in.readInt());
+			String repository = (String)in.readObject();
+			if(repository!=null && repository.equals(ConfigManager.getParm("schemastore")))
+				try {
+					String item = (String)in.readObject();
+					while(1==1) { load(in,item); item = (String)in.readObject(); }
+				} catch(EOFException e) {}
 		}
-		catch(Exception e) {}
+		catch(Exception e) { System.out.println("(E) ProjectManager - " + e.getMessage()); }
 		if(in != null) try { in.close(); } catch(Exception e) {}
-		
-		// Only open mapping if pointing to same repository and repository still contains schemas
-		if(repository!=null && repository.equals(ConfigManager.getParm("schemastore")))
-		{
-			mappingManager.setMapping(mapping);
 
-			// Set the mapping cells (filtering out all invalid mapping cells)
-			for(MappingCell mappingCell : mappingCells)
-			{
-				mappingCell.setId(null);
-				mappingCellManager.setMappingCell(mappingCell);
-			}
-			
-			// Set the displayed schemas
-			harmonyModel.getSelectedInfo().setSelectedSchemas(leftSchemas, rightSchemas);
-		}
-		if(mapping==null) mappingManager.setMapping(new Mapping());
+		// If no mapping was assigned, set a default mapping
+		if(harmonyModel.getMappingManager().getMapping()==null)
+			harmonyModel.getMappingManager().setMapping(new Mapping());
 		
 		// Monitor for changes to the mapping
-		mappingManager.addListener(this);	
+		harmonyModel.getMappingManager().addListener(this);	
 	}
-	
+		
 	/** Saves the project to file */
 	public void save()
 	{
 		File tempProjectFile = new File(projectFile.getParentFile(),projectFile.getName()+".tmp");
 		try
 		{
-			// Create a temporary file to output to
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(tempProjectFile));
-			
-			// Store the repository, mapping, and if saved to repository
+			// Save all project settings
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(tempProjectFile));			
 			out.writeObject(ConfigManager.getParm("schemastore"));
-			out.writeObject(harmonyModel.getMappingManager().getMapping());
-			out.writeBoolean(harmonyModel.getMappingManager().isModified());
-			
-			// Store the mapping cells
-			ArrayList<MappingCell> mappingCells = harmonyModel.getMappingCellManager().getMappingCells();
-			out.writeInt(mappingCells.size());
-			for(MappingCell mappingCell : mappingCells)
-				out.writeObject(mappingCell);
-			
-			// Store the schemas displayed on the left
-			ArrayList<Integer> leftSchemas = harmonyModel.getSelectedInfo().getSchemas(HarmonyConsts.LEFT);
-			out.writeInt(leftSchemas.size());
-			for(Integer leftSchema : leftSchemas) out.writeInt(leftSchema);
-
-			// Stores the schemas displayed on the right
-			ArrayList<Integer> rightSchemas = harmonyModel.getSelectedInfo().getSchemas(HarmonyConsts.RIGHT);
-			out.writeInt(rightSchemas.size());
-			for(Integer rightSchema : rightSchemas) out.writeInt(rightSchema);
+			save(out,"Mapping");
+			save(out,"MappingCells");
+			save(out,"MappingModified");
+			save(out,"DisplayedSchemas");
 			out.close();			
 			
 			// Replace the tool file with the new tool file
