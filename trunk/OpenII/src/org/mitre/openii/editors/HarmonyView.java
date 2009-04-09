a7892a5ff4a5fd2395cb0e48c74c4f509295e2a5
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-04-09 13:32:54 +0000

    Finish setting up the Harmony view within OpenII

--- trunk/OpenII/src/org/mitre/openii/editors/HarmonyView.java
+++ trunk/OpenII/src/org/mitre/openii/editors/HarmonyView.java
@@ -9,12 +9,43 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.mitre.harmony.model.HarmonyModel;
 import org.mitre.harmony.model.SchemaStoreManager;
+import org.mitre.harmony.view.dialogs.schemas.SchemaDialog;
 import org.mitre.harmony.view.harmonyPane.HarmonyFrame;
 import org.mitre.openii.model.OpenIIManager;
 
 /** Constructs the Harmony View */
 public class HarmonyView extends OpenIIEditor
 {
+	/** Private class for running the applet */ @SuppressWarnings("serial")
+	private class HarmonyApplet extends JApplet
+	{
+		/** Stores the Harmony model */
+		private HarmonyModel harmonyModel;
+		
+		/** Private variable to indicate if initialization has been completed */
+		private boolean initializationCompleted = false;
+
+		/** Constructs the applet */
+		private HarmonyApplet(Frame frame)
+		{
+			harmonyModel = new HarmonyModel(frame);
+			harmonyModel.getMappingManager().loadMapping(elementID);
+			HarmonyFrame harmonyFrame = new HarmonyFrame(harmonyModel);
+			setContentPane(harmonyFrame);
+		}
+		
+		/** Displays the schema dialog on initialization */
+		public void repaint(long time, int x, int y, int width, int height)
+		{
+			super.repaint(time, x, y, width, height);
+			if(!initializationCompleted)
+			{
+				new SchemaDialog(harmonyModel);
+				initializationCompleted = true;
+			}
+		}
+	}
+	
 	/** Displays the Harmony View */
 	public void createPartControl(Composite parent)
 	{
@@ -24,19 +55,6 @@ public class HarmonyView extends OpenIIEditor
 		// Constructs the AWT frame
 		Composite swtAwtComponent = new Composite(parent, SWT.EMBEDDED);
 		Frame frame = SWT_AWT.new_Frame(swtAwtComponent);
-
-		// Construct the applet pane containing the Harmony frame
-		JApplet appletPane = new JApplet();
-		HarmonyModel harmonyModel = new HarmonyModel(frame);
-		harmonyModel.getMappingManager().loadMapping(elementID);
-		appletPane.add(new HarmonyFrame(harmonyModel));
-		frame.add(appletPane);
+		frame.add(new HarmonyApplet(frame));
 	}
-
-	/** Shuts down the Harmony View
-	public void dispose()
-	{
-		ProjectManager.save();
-		super.dispose();
-	}*/
 }
