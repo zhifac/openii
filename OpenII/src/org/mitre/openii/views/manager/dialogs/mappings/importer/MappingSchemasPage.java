578d161ddb8b7cf91d929c6a3306b64a886dce5c
cwolf <cwolf@mitre.org>
2009-05-13 20:13:54 +0000

    Add Mapping Importer to OpenII

---
+++ OpenII/src/org/mitre/openii/views/manager/dialogs/mappings/importer/MappingSchemasPage.java
@@ -0,0 +1,161 @@
+package org.mitre.openii.views.manager.dialogs.mappings.importer;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.mitre.openii.model.OpenIIManager;
+import org.mitre.openii.views.manager.dialogs.DialogComponents;
+import org.mitre.schemastore.model.Schema;
+
+/**
+ * Class for displaying the Import Mapping page
+ */
+public class MappingSchemasPage extends WizardPage
+{
+	/** Private class for displaying the selection pane */
+	private class SelectionPane implements ISelectionChangedListener, KeyListener
+	{
+		/** Stores the schema list */
+		private ComboViewer schemaList = null;
+		
+		/** Constructs the selection pane */
+		private SelectionPane(Composite parent, Schema mappingSchema)
+		{
+			// Display the mapping schema name
+			DialogComponents.createLabel(parent, mappingSchema.getName());
+			
+			// Display the list of schemas which might align with the mapping schema
+			schemaList = new ComboViewer(parent, SWT.NONE);
+			for(Schema currSchema : OpenIIManager.getSchemas())
+				schemaList.add(currSchema);
+
+			// Tries to select the proper schema if possible
+			for(int i=0; i<schemaList.getCombo().getItemCount(); i++)
+			{
+				Schema schema = (Schema)schemaList.getElementAt(i);
+				if(schema.getName().equals(mappingSchema.getName()))
+					schemaList.getCombo().select(i);
+				schemaList.addSelectionChangedListener(this);
+				schemaList.getCombo().addKeyListener(this);
+			}
+		}
+
+		/** Returns the selected schema ID */
+		private Integer getSchemaID()
+		{
+			Integer index = schemaList.getCombo().getSelectionIndex();
+			if(index>0) return ((Schema)schemaList.getElementAt(index)).getId();
+			return null;
+		}
+		
+		/** Monitors changes to the selected schema */
+		public void selectionChanged(SelectionChangedEvent e)
+			{ updatePageCompleteStatus(); }
+
+		/** Monitors changes to the selected schema */
+		public void keyPressed(KeyEvent e)
+			{ updatePageCompleteStatus(); }
+
+		// Unused listener events
+		public void keyReleased(KeyEvent e) {}
+	}
+	
+	/** Stores the current URI being referenced */
+	private URI currentURI = null;
+	
+	/** Stores a list of all selection panes */
+	private ArrayList<SelectionPane> selectionPanes = new ArrayList<SelectionPane>();
+	
+	/** Stores the scrolled pane */
+	private ScrolledComposite scrolledPane = null;
+	
+	/** Constructor for the Import Mappings pane */
+	public MappingSchemasPage()
+	{
+		super("SchemasPage");
+		setTitle("Select Schemas");
+		setDescription("Select the schemas associated with the mapping");
+	}
+	
+	/** Constructs the Mappings Schema page */
+	public void createControl(Composite parent)
+	{
+		// Construct the main pane
+		Composite pane = new Composite(parent, SWT.NONE);
+		pane.setLayout(new GridLayout(1,false));
+		pane.setLayoutData(new GridData(GridData.FILL_BOTH));
+		setControl(pane);
+
+		// Construct the group pane for showing the schemas to map
+		Group group = new Group(pane, SWT.NONE);
+		group.setText("Schemas");
+		group.setLayout(new GridLayout(1,false));
+		group.setLayoutData(new GridData(GridData.FILL_BOTH));
+	
+		// Construct the scrolling pane for showing the schemas to map
+		scrolledPane = new ScrolledComposite(group, SWT.V_SCROLL);
+		scrolledPane.setLayoutData(new GridData(GridData.FILL_BOTH));
+	}
+
+	/** Initializes the Mapping Schema page as needed */
+	public void setVisible(boolean visible)
+	{
+		// Updates the page if the current URI has changed
+		MappingPropertiesPage propertiesPage = ((ImportMappingWizard)getWizard()).getPropertiesPage();
+		if(!propertiesPage.getURI().equals(currentURI))
+		{
+			// Update the current URI
+			currentURI = propertiesPage.getURI();
+
+			// Replace the displayed schemas to reflect the new URI
+			Composite schemaPane = new Composite(scrolledPane, SWT.NONE);
+			schemaPane.setLayout(new GridLayout(2,false));
+			try {
+				for(Schema schema : propertiesPage.getImporter().getSchemas(currentURI))
+					selectionPanes.add(new SelectionPane(schemaPane,schema));
+			} catch(Exception e) {}
+			
+			// Refresh the scroll pane to display the updated schemas
+			scrolledPane.setContent(schemaPane);
+			scrolledPane.setExpandVertical(true);
+			scrolledPane.setExpandHorizontal(true);
+			scrolledPane.setMinSize(schemaPane.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+			
+			// Check page completion
+			updatePageCompleteStatus();
+		}
+			
+		// Make visible
+		super.setVisible(visible);
+	}
+	
+	/** Returns the selected list of schema IDs */
+	ArrayList<Integer> getSchemaIDs()
+	{
+		ArrayList<Integer> schemaIDs = new ArrayList<Integer>();
+		for(SelectionPane selectionPane : selectionPanes)
+			schemaIDs.add(selectionPane.getSchemaID());
+		return schemaIDs;
+	}
+	
+	/** Checks completion of page */
+	private void updatePageCompleteStatus()
+	{
+		boolean completed = true;
+		for(SelectionPane selectionPane : selectionPanes)
+			if(selectionPane.getSchemaID()==null) completed = false;
+		setPageComplete(completed);	
+	}
+}
