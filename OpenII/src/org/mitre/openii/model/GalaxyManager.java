1771426531e7c27d275a54b82a910827db5d1e3d
cwolf <cwolf@mitre.org>
2009-05-15 14:30:59 +0000

    Fix Galaxy refresh in OpenII

--- OpenII/src/org/mitre/openii/model/GalaxyManager.java
+++ OpenII/src/org/mitre/openii/model/GalaxyManager.java
@@ -14,7 +14,25 @@ import org.mitre.schemastore.model.Schema;
 /** Class for managing Galaxy */
 public class GalaxyManager
 {
-	/** Initializes Galaxy */
+	/** Private class for managing changes to OpenII */
+	static private class OpenIIMonitor implements OpenIIListener
+	{
+		// Reset schema information as needed
+		public void schemaAdded(Integer schemaID) { Schemas.resetSchema(schemaID); }
+		public void schemaDeleted(Integer schemaID) { Schemas.resetSchema(schemaID); }
+		public void schemaModified(Integer schemaID) { Schemas.resetSchema(schemaID); }
+
+		// Unused listener events
+		public void groupAdded(Integer groupID) {}
+		public void groupDeleted(Integer groupID) {}
+		public void groupModified(Integer groupID) {}
+		public void mappingAdded(Integer mappingID) {}
+		public void mappingDeleted(Integer mappingID) {}
+		public void mappingModified(Integer mappingID) {}		
+	}
+	static private OpenIIMonitor monitor = new OpenIIMonitor();
+	
+	/** Constructs the Galaxy Manager */
 	static
 	{
 		// Initialize the connection to SchemaStore
@@ -25,6 +43,9 @@ public class GalaxyManager
 		ArrayList<Schema> schemas = Schemas.getSchemas();
 		if(schemas.size()>0)
 			SelectedObjects.setSelectedSchema(schemas.get(0).getId());
+		
+		// Monitors changes in OpenII
+		OpenIIManager.addListener(monitor);
 	}
 
 	/** Returns the Galaxy Extension Pane */
