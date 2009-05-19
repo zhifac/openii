a6830c5838d65576f101321304cf9dd32ccf4dab
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-05-19 00:08:24 +0000

    Move listener calls into Harmony Model from individual manager

--- trunk/Harmony/src/org/mitre/harmony/model/HarmonyModel.java
+++ trunk/Harmony/src/org/mitre/harmony/model/HarmonyModel.java
@@ -6,6 +6,7 @@ import org.mitre.harmony.model.filters.FilterManager;
 import org.mitre.harmony.model.mapping.MappingCellManager;
 import org.mitre.harmony.model.mapping.MappingManager;
 import org.mitre.harmony.model.preferences.PreferencesManager;
+//import org.mitre.harmony.model.search.SearchManager;
 import org.mitre.harmony.model.selectedInfo.SelectedInfoManager;
 
 /** Class for monitoring for changes in the project */
@@ -23,6 +24,7 @@ public class HarmonyModel
 	protected FilterManager filterManager = new FilterManager(this);
 	protected PreferencesManager preferencesManager = new PreferencesManager(this);
 	protected SelectedInfoManager selectedInfoManager = new SelectedInfoManager(this);
+//	protected SearchManager searchManager = new SearchManager(this);
 
 	/** Constructs the Harmony model */
 	public HarmonyModel(Frame baseFrame)
@@ -32,10 +34,13 @@ public class HarmonyModel
 		// Add listeners to the various model objects
 		filterManager.addListener(selectedInfoManager);
 		selectedInfoManager.addListener(filterManager);
-		mappingManager.addListener(mappingCellManager);
+//		selectedInfoManager.addListener(searchManager);
 		preferencesManager.addListener(mappingCellManager);
+		mappingManager.addListener(mappingCellManager);
 		mappingManager.addListener(preferencesManager);
+		mappingManager.addListener(selectedInfoManager);
 		mappingCellManager.addListener(mappingManager);
+		mappingCellManager.addListener(selectedInfoManager);
 	}
 	
 	/** Returns the base frame */
@@ -54,6 +59,10 @@ public class HarmonyModel
 	public SelectedInfoManager getSelectedInfo()
 		{ return selectedInfoManager; }
 
+	/** Returns the search manager */
+//	public SearchManager getSearchManager()
+//		{ return searchManager; }
+	
 	/** Returns the schema manager */
 	public SchemaManager getSchemaManager()
 		{ return schemaManager; }
