f6a93d58fff6bf6fb8864f336ea079a4233aa5f2
cwolf <cwolf@mitre.org>
2009-05-26 17:46:35 +0000

    Add Mapping View to OpenII

--- OpenII/src/org/mitre/openii/editors/SchemaView.java
+++ OpenII/src/org/mitre/openii/editors/SchemaView.java
@@ -3,17 +3,13 @@ package org.mitre.openii.editors;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.jar.Attributes;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.ExpandBar;
-import org.eclipse.swt.widgets.ExpandItem;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
-import org.mitre.openii.application.OpenIIActivator;
 import org.mitre.openii.model.OpenIIManager;
+import org.mitre.openii.widgets.ExpandBarWidgets;
 import org.mitre.schemastore.model.Alias;
 import org.mitre.schemastore.model.Attribute;
 import org.mitre.schemastore.model.Containment;
@@ -21,13 +17,14 @@ import org.mitre.schemastore.model.Domain;
 import org.mitre.schemastore.model.DomainValue;
 import org.mitre.schemastore.model.Entity;
 import org.mitre.schemastore.model.Relationship;
+import org.mitre.schemastore.model.Schema;
 import org.mitre.schemastore.model.SchemaElement;
 import org.mitre.schemastore.model.Subtype;
 import org.mitre.schemastore.model.graph.Graph;
 
 /** Constructs the Schema Graph */
 public class SchemaView extends OpenIIEditor
-{
+{	
 	/** Returns the list of schema elements for the specified element type */
 	private ArrayList<SchemaElement> getElements(Graph graph, Class<?> type)
 	{
@@ -44,73 +41,28 @@ public class SchemaView extends OpenIIEditor
 		return elements;
 	}
 	
-	/** Generates a table */
-	private Table createTable(Composite parent, String fields[])
-	{
-		// Generate the table
-		Table table = new Table(parent, SWT.SINGLE | SWT.BORDER | SWT.FULL_SELECTION);
-		table.setLinesVisible (true);
-		table.setHeaderVisible (true);
-		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-		table.setLayoutData(data);
-		
-		// Specify the field names
-		for(String field : fields)
-			new TableColumn(table, SWT.NONE).setText(field);
-		
-		// Return the table
-		return table;
-	}
-	
-	/** Generates a table item */
-	private void createTableItem(Table table, Object values[])
-	{
-		TableItem item = new TableItem(table, SWT.NONE);
-		for(int i=0; i<values.length; i++)
-			item.setText(i,values[i]==null ? "" : values[i].toString());
-	}
-	
-	/** Generates an expand item */
-	private void createExpandItem(ExpandBar bar, String label, Composite item)
-	{
-		// Determine which height to use for sizing the expand item
-		Integer height = item.computeSize(SWT.DEFAULT, SWT.DEFAULT).y;
-		if(height>250) height = 250;
-		
-		// Generate the expand item
-		ExpandItem expandItem = new ExpandItem(bar, SWT.NONE);
-		expandItem.setText(label);
-		expandItem.setHeight(height);
-		expandItem.setControl(item);
-		expandItem.setImage(OpenIIActivator.getImage("SchemaElement.jpg"));
-		expandItem.setExpanded(true);
-	}
-	
-	/** Displays the Schema Graph */
-	public void createPartControl(Composite parent)
+	/** Generate the schema element panes */
+	private void generateElementPanes(ExpandBar bar)
 	{
 		// Get the schema graph
 		Graph graph = OpenIIManager.getGraph(elementID);
-		
-		// Create the expand bar
-		ExpandBar bar = new ExpandBar(parent, SWT.V_SCROLL);
-		bar.setSpacing(8);
 
 		// Generate the entities table
-		Table table = createTable(bar,new String[]{"ID","Name","Description"});
+		String[] fields = new String[]{"ID","Name","Description"};
+		ArrayList<Object[]> rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,Entity.class))
 		{
 			Entity entity = (Entity)element;
 			Integer id = entity.getId();
 			String name = entity.getName();
 			String description = entity.getDescription();
-			createTableItem(table,new Object[]{id,name,description});
+			rows.add(new Object[]{id,name,description});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Entities",table);
+		ExpandBarWidgets.createTablePane(bar, "Entities", fields, rows);
 
 		// Generate the attributes table
-		table = createTable(bar,new String[]{"ID","Name","Entity","Domain","Min","Max","IsKey","Description"});
+		fields = new String[]{"ID","Name","Entity","Domain","Min","Max","IsKey","Description"};
+		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,Attribute.class))
 		{
 			Attribute attribute = (Attribute)element;
@@ -122,26 +74,26 @@ public class SchemaView extends OpenIIEditor
 			Integer max = attribute.getMax();
 			Boolean isKey = attribute.isKey();
 			String description = attribute.getDescription();
-			createTableItem(table,new Object[]{id,name,entity,domain,min,max,isKey,description});
+			rows.add(new Object[]{id,name,entity,domain,min,max,isKey,description});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Attributes",table);
+		ExpandBarWidgets.createTablePane(bar, "Attributes", fields, rows);
 
 		// Generate the domains table
-		table = createTable(bar,new String[]{"ID","Name","Description"});
+		fields = new String[]{"ID","Name","Description"};
+		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,Domain.class))
 		{
 			Domain domain = (Domain)element;
 			Integer id = domain.getId();
 			String name = domain.getName();
 			String description = domain.getDescription();
-			createTableItem(table,new Object[]{id,name,description});
+			rows.add(new Object[]{id,name,description});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Domains",table);		
+		ExpandBarWidgets.createTablePane(bar, "Domain", fields, rows);
 
 		// Generate the domain values table
-		table = createTable(bar,new String[]{"ID","Name","Domain","Description"});
+		fields = new String[]{"ID","Name","Domain","Description"};
+		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,DomainValue.class))
 		{
 			DomainValue domainValue = (DomainValue)element;
@@ -149,13 +101,13 @@ public class SchemaView extends OpenIIEditor
 			String name = domainValue.getName();
 			String domain = graph.getElement(domainValue.getDomainID()).getName() + " (" + domainValue.getDomainID() + ")";
 			String description = domainValue.getDescription();
-			createTableItem(table,new Object[]{id,name,domain,description});
+			rows.add(new Object[]{id,name,domain,description});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Domain Values",table);		
+		ExpandBarWidgets.createTablePane(bar, "Domain Values", fields, rows);
 
 		// Generate the relationships table
-		table = createTable(bar,new String[]{"ID","Name","Left Element","Left Min","Left Max","Right Element","Right Min","Right Max","Description"});
+		fields = new String[]{"ID","Name","Left Element","Left Min","Left Max","Right Element","Right Min","Right Max","Description"};
+		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,Relationship.class))
 		{
 			Relationship relationship = (Relationship)element;
@@ -168,54 +120,74 @@ public class SchemaView extends OpenIIEditor
 			Integer rightMin = relationship.getRightMin();
 			Integer rightMax = relationship.getRightMax();
 			String description = relationship.getDescription();
-			createTableItem(table,new Object[]{id,name,left,leftMin,leftMax,right,rightMin,rightMax,description});
+			rows.add(new Object[]{id,name,left,leftMin,leftMax,right,rightMin,rightMax,description});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Relationships",table);		
+		ExpandBarWidgets.createTablePane(bar, "Relationships", fields, rows);
 
 		// Generate the containments table
-		table = createTable(bar,new String[]{"ID","Name","Parent Element","Child Element","Min","Max","Description"});
+		fields = new String[]{"ID","Name","Parent Element","Child Element","Min","Max","Description"};
+		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,Containment.class))
 		{
 			Containment containment = (Containment)element;
 			Integer id = containment.getId();
 			String name = containment.getName();
 			String parentElement = containment.getParentID()==null ? "" : graph.getElement(containment.getParentID()).getName() + " (" + containment.getParentID() + ")";
-			String childElement = graph.getElement(containment.getChildID()).getName() + " (" + containment.getChildID() + ")";
+			String childElement = graph.getElement(containment.getChildID()).getName() + " (" + containment.getParentID() + ")";
 			Integer min = containment.getMin();
 			Integer max = containment.getMax();
 			String description = containment.getDescription();
-			createTableItem(table,new Object[]{id,name,parentElement,childElement,min,max,description});
+			rows.add(new Object[]{id,name,parentElement,childElement,min,max,description});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Containments",table);		
+		ExpandBarWidgets.createTablePane(bar, "Containments", fields, rows);
 
 		// Generate the subtypes table
-		table = createTable(bar,new String[]{"ID","Name","Parent Element","Child Element","Description"});
+		fields = new String[]{"ID","Name","Parent Element","Child Element","Description"};
+		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,Subtype.class))
 		{
 			Subtype subtype = (Subtype)element;
 			Integer id = subtype.getId();
 			String name = subtype.getName();
 			String parentElement = graph.getElement(subtype.getParentID()).getName() + " (" + subtype.getParentID() + ")";
-			String childElement = graph.getElement(subtype.getChildID()).getName() + " (" + subtype.getChildID() + ")";
+			String childElement = graph.getElement(subtype.getChildID()).getName() + " (" + subtype.getParentID() + ")";
 			String description = subtype.getDescription();
-			createTableItem(table,new Object[]{id,name,parentElement,childElement,description});
+			rows.add(new Object[]{id,name,parentElement,childElement,description});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Subtypes",table);		
+		ExpandBarWidgets.createTablePane(bar, "Subtypes", fields, rows);
 
 		// Generate the aliases table
-		table = createTable(bar,new String[]{"ID","Name","Aliased Element"});
+		fields = new String[]{"ID","Name","Aliased Element"};
+		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(graph,Alias.class))
 		{
 			Alias alias = (Alias)element;
 			Integer id = alias.getId();
 			String name = alias.getName();
 			String aliasedElement = graph.getElement(alias.getElementID()).getName() + " (" + alias.getElementID() + ")";
-			createTableItem(table,new Object[]{id,name,aliasedElement});
+			rows.add(new Object[]{id,name,aliasedElement});
 		}
-		for(int i=0; i<table.getColumnCount(); i++) table.getColumn(i).pack();
-		createExpandItem(bar,"Aliases",table);		
+		ExpandBarWidgets.createTablePane(bar, "Aliases", fields, rows);
+	}
+	
+	/** Displays the Schema Graph */
+	public void createPartControl(Composite parent)
+	{		
+		// Create the expand bar
+		ExpandBar bar = new ExpandBar(parent, SWT.V_SCROLL);
+		bar.setSpacing(8);
+
+		// Creates the properties pane
+		Attributes attributes = new Attributes();
+		Schema schema = OpenIIManager.getSchema(elementID);
+		attributes.putValue("Name", schema.getName());
+		attributes.putValue("Author", schema.getAuthor());
+		attributes.putValue("Source", schema.getSource());
+		attributes.putValue("Type", schema.getType());
+		attributes.putValue("Description", schema.getDescription());
+		ExpandBarWidgets.createPropertiesPane(bar,attributes);
+		
+		// Generate the element panes in the schema view
+		generateElementPanes(bar);
 	}
 }
