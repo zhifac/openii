6af560d434299e7322d7e5236f049e648168929a
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-05-28 13:22:48 +0000

    Reinstall the matchmaker files

---
+++ trunk/SchemaStoreClient/src/org/mitre/schemastore/porters/mappingExporters/matchmaker/groupE.java
@@ -0,0 +1,103 @@
+package org.mitre.schemastore.porters.mappingExporters.matchmaker;
+
+import java.util.ArrayList;
+
+/**
+ * A groupE is just an arraylist of Nodes, that can do a few tricks. This object
+ * came from Michael Morse's clustering code, with lots of modifications by
+ * David and Maya. The way we actually use it in our code, a groupE is a
+ * cluster. The hierarchical clustering code's result is a list of groupEs,
+ * which are "chains" of pairwise matches that all logically group together.
+ * 
+ * @author MDMORSE
+ * 
+ */
+public class groupE implements Comparable<groupE> {
+        public ArrayList<SchemaElementNode> nodes;
+
+        public SchemaElementNode leastNode = null;
+
+        public groupE(SchemaElementNode n) {
+                nodes = new ArrayList<SchemaElementNode>();
+                add(n);
+        }
+
+        void add(SchemaElementNode n) {
+                nodes.add(n);
+                updateLeastNode(n);
+        }
+
+        private void updateLeastNode(SchemaElementNode n) {
+                if (leastNode == null || leastNode.compareTo(n) > 0) leastNode = n;
+        }
+
+        /**
+         * full linkage. max distance is 1, least is 0 Simple Linkage
+         * 
+         * @param two
+         * @return the largest distance between two groupE nodes
+         */
+        float completeLinkage(groupE two) {
+                int i;
+                int j;
+                float maxDist = 0;
+                for (i = 0; i < this.nodes.size(); i++) {
+                        Node t_node = this.nodes.get(i);
+                        for (j = 0; j < two.nodes.size(); j++) {
+                                Node s_node = two.nodes.get(j);
+                                int m = t_node.pointers.indexOf(s_node);
+                                if (m == -1) continue;
+                                // return 0;
+                                float st_dist = t_node.distances.get(m).floatValue();
+                                if (maxDist < st_dist) {
+                                        maxDist = st_dist;
+                                }
+                        }
+                }
+                return maxDist;
+        }
+
+        // Returns minimum distance between two nodes
+        float simpleLinkage(groupE two) {
+                int i, j;
+                float minDist = 1;
+                for (i = 0; i < this.nodes.size(); i++) {
+                        Node t_node = this.nodes.get(i);
+                        for (j = 0; j < two.nodes.size(); j++) {
+                                Node s_node = two.nodes.get(j);
+                                int m = t_node.pointers.indexOf(s_node);
+                                if (m == -1) return 0;
+                                float st_dist = t_node.distances.get(m).floatValue();
+                                if (minDist > st_dist) minDist = st_dist;
+                        }
+                }
+                return minDist;
+        }
+
+        void groupEcombine(groupE two) {
+                nodes.ensureCapacity(nodes.size() + two.nodes.size());
+                nodes.addAll(two.nodes);
+                updateLeastNode(two.leastNode);
+        }
+
+        public ArrayList<SchemaElementNode> getGroup() {
+                return nodes;
+        }
+
+        public int compareTo(groupE other) {
+                return this.leastNode.compareTo(other.leastNode);
+        }
+
+        /**
+         * returns the schemaelement node that belongs to the specified schema
+         * 
+         * @param baseSchema
+         * @return SchemaElementNode
+         */
+        public SchemaElementNode getNode(Integer baseSchema) {
+                for (SchemaElementNode n : nodes)
+                        if (n.schemaId == baseSchema) return n ;
+                return null;
+        }
+
+}
