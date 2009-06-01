2a322b8941242a3cf16d12fcca3d40246d391f6e
jchoyt <jchoyt@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-06-01 14:51:19 +0000

    start of mapping functions

---
+++ SchemaStore/src/org/mitre/schemastore/model/mapfunctions/NotImplementedException.java
@@ -0,0 +1,14 @@
+// Copyright 2008 The MITRE Corporation. ALL RIGHTS RESERVED.
+
+package org.mitre.schemastore.model.mapfunctions;
+
+public class NotImplementedException extends Exception
+{
+    public NotImplementedException(String message)
+    {
+        super(message);
+    }
+}
+
+// Please do not remove the line below - jch
+// :wrap=soft:noTabs=true:collapseFolds=1:maxLineLen=120:mode=java:tabSize=4:indentSize=4:noWordSep=_:folding=indent:
