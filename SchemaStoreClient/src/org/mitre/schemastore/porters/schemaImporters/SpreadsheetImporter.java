bb0015f917bb7508b3073b81c2020bd84f408d2e
cwolf <cwolf@mitre.org>
2009-06-12 21:07:34 +0000

    Add in ability for schema importer to provide schema information

--- SchemaStoreClient/src/org/mitre/schemastore/porters/schemaImporters/SpreadsheetImporter.java
+++ SchemaStoreClient/src/org/mitre/schemastore/porters/schemaImporters/SpreadsheetImporter.java
@@ -252,12 +252,12 @@ public class SpreadsheetImporter extends SchemaImporter {
 	}
 
 	/** Returns the list of schemas which this schema extends */
-	protected ArrayList<Integer> getExtendedSchemaIDs() throws ImporterException {
+	protected ArrayList<Integer> generateExtendedSchemaIDs() throws ImporterException {
 		return new ArrayList<Integer>();
 	}
 
 	/** Generate the schema elements */
-	public ArrayList<SchemaElement> getSchemaElements() throws ImporterException {
+	public ArrayList<SchemaElement> generateSchemaElements() throws ImporterException {
 		generate();
 		ArrayList<SchemaElement> elements = generateSchemaElementList();
 		// Schema schema = new Schema(nextId(),"thisschema","me","","","description",false);
