55bd3e52e03f0f5f41dbfc3898fa641880c33fc7
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-06-19 20:05:17 +0000

    Move the "Edit Schemas" pane to be the "Properties" pane

---
+++ trunk/Harmony/src/org/mitre/harmony/view/dialogs/properties/SchemaModelItem.java
@@ -0,0 +1,65 @@
+package org.mitre.harmony.view.dialogs.properties;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.mitre.harmony.model.HarmonyModel;
+import org.mitre.schemastore.model.Schema;
+import org.mitre.schemastore.model.graph.model.GraphModel;
+import org.mitre.schemastore.model.graph.HierarchicalGraph;
+
+/** Displays a schema item with the model setting */
+class SchemaModelItem extends JPanel
+{
+	// Constants to define which side the schema should be displayed on
+	static final int NONE = 0;
+	static final int LEFT = 1;
+	static final int RIGHT = 2;
+	
+	/** Stores the schema for this schema item */
+	private Schema schema;
+
+	// Stores the model selection for the specific schema
+	private JComboBox modelSelection = new JComboBox();
+	
+	/** Constructs the schema model item */
+	SchemaModelItem(Schema schema, HarmonyModel harmonyModel)
+	{
+		this.schema = schema;
+		
+		// Initialize the model selection box
+		modelSelection.setOpaque(false);
+		modelSelection.setPreferredSize(new Dimension(80,20));
+		modelSelection.addItem("<Default>");
+		for(GraphModel model : HierarchicalGraph.getGraphModels())
+			modelSelection.addItem(model);
+
+		// Set the selected model
+		GraphModel selectedModel = harmonyModel.getMappingManager().getGraphModel(schema.getId());
+		if(selectedModel!=null)
+			modelSelection.setSelectedItem(selectedModel);
+		
+		// Constructs the schema item
+		setBorder(new EmptyBorder(3,0,3,0));
+		setMaximumSize(new Dimension(10000,26));
+		setOpaque(false);
+		setFocusable(false);
+		setLayout(new BorderLayout());
+		add(new SchemaModelRow(modelSelection,new JLabel(schema.getName())));
+	}
+	
+	/** Returns the schema associated with this item */
+	Schema getSchema() { return schema; }
+	
+	/** Returns the graph model associated with this item */
+	GraphModel getModel()
+	{
+		Object model = modelSelection.getSelectedItem();
+		return (model instanceof GraphModel) ? (GraphModel)model : null;
+	}
+}
