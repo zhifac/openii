cbfcf5569ef94818b5c742803660355c897decd5
kuangc <kuangc@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-06-21 22:20:36 +0000

    fixed search result listing problem

--- SchemrWeb/src/org/openii/schemrserver/servlet/SessionState.java
+++ SchemrWeb/src/org/openii/schemrserver/servlet/SessionState.java
@@ -1,5 +1,8 @@
 package org.openii.schemrserver.servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openii.schemrserver.search.MatchSummary;
 import org.openii.schemrserver.search.Query;
 
@@ -10,12 +13,14 @@ public class SessionState {
 
 	public Query query;
 	
-	public MatchSummary[] matchSummaries;
+//	public MatchSummary[] matchSummaries;
+	public Map<Integer, MatchSummary> idToMatchSummary;
 
 	public String[] results;
 
 	public SessionState(String id) {
 		this.sessionId = id;
+		this.idToMatchSummary = new HashMap<Integer, MatchSummary>();
 	}
 
 
