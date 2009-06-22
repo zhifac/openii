26c5e7f9ef132f83bcb1a5de1046b0cebb68b619
kuangc <kuangc@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-06-22 15:26:13 +0000

    file upload not working yet, refactored out db strings to Const in schemastore client

--- trunk/SchemrWeb/flex_src/Consts.as
+++ trunk/SchemrWeb/flex_src/Consts.as
@@ -1,14 +1,6 @@
-package
-{
-	public class Consts
-	{
-		private static const COLOR_GRAY:String 	= "#999999";
+package {
+	public class Consts {
 		
-		public static const COLOR_TEXT_DISABLED:String 	= COLOR_GRAY;
-		public static const BUTTON_WIDTH:uint 			= 90;
-		public static const SECONDS_PER_RUN:Number		= 63;
-		
-		public static const NULL_INDEX:String 			= "-1";
-		
+				
 	}
 }
--- trunk/SchemrWeb/src/org/openii/schemrserver/indexer/BuildIndex.java
+++ trunk/SchemrWeb/src/org/openii/schemrserver/indexer/BuildIndex.java
@@ -8,38 +8,33 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 
+import org.mitre.schemastore.client.Consts;
 import org.openii.schemrserver.indexer.SchemaStoreIndex;
 import org.openii.schemrserver.indexer.SchemaUtility;
 
 public class BuildIndex {
-	public static String TEST_INDEX_DIR = SchemaUtility.LOCAL_INDEX_DIR;
+	public static String TEST_INDEX_DIR = Consts.LOCAL_INDEX_DIR;
 	
 	public static void main(String[] args) throws CorruptIndexException, IOException{
-		System.out.println("Building index in "+SchemaUtility.LOCAL_INDEX_DIR);
+		System.out.println("Building index in "+Consts.LOCAL_INDEX_DIR);
 		Analyzer analyzer = new StandardAnalyzer();
 		IndexWriter iwriter;
 		try {
-			iwriter = new IndexWriter(SchemaUtility.LOCAL_INDEX_DIR, analyzer, true);
+			iwriter = new IndexWriter(Consts.LOCAL_INDEX_DIR, analyzer, true);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return;
 		} 
 		try {
 			SchemaStoreIndex.createIndex(iwriter, SchemaUtility.getCLIENT());
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (CorruptIndexException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		iwriter.optimize();
 		iwriter.close();
-		
-		
 	}
 }
