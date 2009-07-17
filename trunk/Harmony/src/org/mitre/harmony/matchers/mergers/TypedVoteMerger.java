31178fc2ee0f99dce5e244632a04a0b86bfe25e9
jkorb@mitre.org <jkorb@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-07-17 21:08:14 +0000

    Added dialog to allow user to select which types to match to which types.

--- trunk/Harmony/src/org/mitre/harmony/matchers/mergers/TypedVoteMerger.java
+++ trunk/Harmony/src/org/mitre/harmony/matchers/mergers/TypedVoteMerger.java
@@ -8,45 +8,51 @@ import org.mitre.schemastore.model.SchemaElement;
 import org.mitre.schemastore.model.graph.FilteredGraph;
 
 /**
- * Vote merger which only merges together elements of the same type
- * @author CWOLF
+ * Vote merger which only merges together elements types the user specifies
+ * @author JKORB
  */
 public class TypedVoteMerger extends VoteMerger
 {
 	/** Stores a cache of all schema element types */
-	private HashMap<Integer,Class> classMap = new HashMap<Integer,Class>();
-	
+	private HashMap<Integer,String> classMap = new HashMap<Integer,String>();
+
+	private HashMap<String, Integer> typeMap = new HashMap<String,Integer>();
+
 	/** Returns the name associated with this match merger */
 	public String getName()
 		{ return "Typed Vote Merger"; }
 
 	/** Initializes the typed vote merger */
-	public void initialize(FilteredGraph schema1, FilteredGraph schema2)
+	public void initialize(FilteredGraph schema1, FilteredGraph schema2, HashMap<String, Integer> typeMap)
 	{
-		super.initialize(schema1,schema2);
+		this.typeMap = typeMap;
+		super.initialize(schema1,schema2, typeMap);
 
 		// Generate the class map
 		for(SchemaElement element : schema1.getFilteredElements())
-			classMap.put(element.getId(), element.getClass());
+			classMap.put(element.getId(), element.getClass().getName());
 		for(SchemaElement element : schema2.getFilteredElements())
-			classMap.put(element.getId(), element.getClass());
+			classMap.put(element.getId(), element.getClass().getName());
 	}
 	
 	/** Adds voter scores to the vote merger with only same-type (e.g. attribute to attribute) matches */
 	public void addVoterScores(VoterScores voterScores)
 	{
+
 		// Filter out voter scores between elements of different types
 		VoterScores filteredVoterScores = new VoterScores(voterScores.getScoreCeiling());
 		for(ElementPair elementPair : voterScores.getElementPairs())
 		{
 			Integer element1 = elementPair.getElement1();
 			Integer element2 = elementPair.getElement2();
-			Class class1 = classMap.get(element1);
-			Class class2 = classMap.get(element2);
-			if(class1!=null && class1.equals(class2))
+			String class1 = classMap.get(element1);
+			String class2 = classMap.get(element2);
+			String key = class1.toString() + "," + class2.toString();
+//			if(class1!=null && class1.equals(class2))
+			if (typeMap.containsKey(key)) {
 				filteredVoterScores.setScore(element1, element2, voterScores.getScore(elementPair));
+			}
 		}
-
 		// Adds voter scores to the vote merger
 		super.addVoterScores(filteredVoterScores);
 	}
