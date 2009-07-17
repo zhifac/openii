9787b7f865ac8ec34f36c4981f6c8a50529be055
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-07-17 15:41:52 +0000

    Check in more repository management code

--- trunk/OpenII/src/org/mitre/openii/views/repositories/RepositoryView.java
+++ trunk/OpenII/src/org/mitre/openii/views/repositories/RepositoryView.java
@@ -1,63 +1,66 @@
 package org.mitre.openii.views.repositories;
 
-import javax.swing.CellEditor;
-
-import org.eclipse.jface.viewers.CheckboxCellEditor;
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.TreeEditor;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.part.ViewPart;
-import org.mitre.galaxy.model.server.SchemaStoreManager;
 import org.mitre.openii.application.OpenIIActivator;
 import org.mitre.openii.model.RepositoryListener;
 import org.mitre.openii.model.RepositoryManager;
 import org.mitre.openii.views.repositories.menu.RepositoryMenuManager;
+import org.mitre.openii.widgets.WidgetUtilities;
 import org.mitre.schemastore.client.Repository;
 
 /** Constructs the Repository View */
 public class RepositoryView extends ViewPart implements RepositoryListener
 {	
-	/** Stores a reference to the viewer */
-	private TreeViewer viewer = null;
+	/** Stores the tree displayed in the repository view */
+	private Tree tree;
+
+	/** Refreshes the displayed repositories */
+	private void updateRepositoryItems()
+	{
+		// Remove all old repository items
+		for(TreeItem item : tree.getItem(0).getItems())
+			((Button)item.getData()).dispose();
+		tree.getItem(0).removeAll();
+		
+		// Generate the nodes for the available repositories
+		for(Repository repository : WidgetUtilities.sortList(RepositoryManager.getRepositories()))
+		{
+			// Adds a checkbox to the repository tree item
+			Button checkbox = new Button(tree, SWT.CHECK);
+			checkbox.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+			checkbox.setImage(OpenIIActivator.getImage("Repository.gif"));
+			checkbox.setText(repository.getName());
+			checkbox.setData(repository);
+			checkbox.setSelection(true);
+			checkbox.setMenu(new RepositoryMenuManager(repository).createContextMenu(tree));
+			
+			// Places the checkbox next to the tree item
+			TreeItem repositoryItem = new TreeItem(tree.getItem(0), SWT.CHECK);
+			repositoryItem.setData(checkbox);
+			TreeEditor editor = new TreeEditor(tree);
+			editor.grabHorizontal = true;
+			editor.setEditor(checkbox, repositoryItem);			
+		}
+	}
 	
 	/** Constructs the Repository View */
 	public RepositoryView()
 		{ RepositoryManager.addListener(this); }
-	
+
 	/** Displays the Repository View */
 	public void createPartControl(Composite parent)
 	{
-		// Create the tree viewer
-/*		viewer = new TreeViewer(parent, SWT.CHECK);
-		viewer = new TreeViewer(parent, SWT.NONE);
-		viewer.setContentProvider(new RepositoryContentProvider());
-		viewer.setLabelProvider(new RepositoryLabelProvider());
-		viewer.setInput("");
-		
-		TreeItem item = viewer.getTree().getItem(0);
-		TreeEditor editor = new TreeEditor(viewer.getTree());
-		Text text = new Text(viewer.getTree(),SWT.NONE);
-		text.setText("Text");
-		editor.setEditor(text,item);
-		
-		// Add the context menu
-		RepositoryMenuManager menuManager = new RepositoryMenuManager(viewer);
-		Menu menu = menuManager.createContextMenu(viewer.getControl());
-		viewer.getControl().setMenu(menu);
-*/
-		
-		
 		// Generate the tree for displaying available repositories
-		final Tree tree = new Tree(parent, SWT.BORDER);
-		RepositoryMenuManager menuManager = new RepositoryMenuManager(tree);
+		tree = new Tree(parent, SWT.BORDER);
+		RepositoryMenuManager menuManager = new RepositoryMenuManager(null);
 		Menu menu = menuManager.createContextMenu(tree);
 		tree.setMenu(menu);
 		
@@ -67,28 +70,18 @@ public class RepositoryView extends ViewPart implements RepositoryListener
 		item.setText("Repositories");
 		item.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
 
-		// Generate the nodes for the available repositories
-		for(Repository repository : RepositoryManager.getRepositories())
-		{
-			TreeItem subItem = new TreeItem(item, SWT.CHECK);
-			Button combo = new Button(tree, SWT.CHECK);
-			combo.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
-			combo.setImage(OpenIIActivator.getImage("Repository.gif"));
-			combo.setText(repository.getName());
-			combo.setSelection(true);
-
-			TreeEditor editor = new TreeEditor(tree); // tree_tasks in my tree
-			editor.grabHorizontal = false;
-editor.horizontalAlignment = SWT.LEFT;
-			editor.setEditor(combo, subItem);
-		}
+		// Updates the repository items
+		updateRepositoryItems();
 	}
+
+	/** Adds a repository to the repository list */
+	public void repositoryAdded(Repository repository)
+		{ updateRepositoryItems(); }
+	
+	/** Removes a repository from the repository list */
+	public void repositoryDeleted(Repository repository)
+		{ updateRepositoryItems(); }
 	
 	// Sets the focus in this view
 	public void setFocus() {}
-
-	// Handles modifications of objects in the Repository tree
-	public void repositoryAdded(Repository repository) { viewer.refresh(); }
-	public void repositoryDeleted(Repository repository) { viewer.refresh(); }
-	public void repositoryModified(Repository repository) { viewer.refresh(); }
 }
