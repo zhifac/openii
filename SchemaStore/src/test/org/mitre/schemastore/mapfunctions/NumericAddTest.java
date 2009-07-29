cbdee513d73d54d6ceb5dd4a97011e6d4aa32540
jchoyt <jchoyt@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-07-29 19:46:40 +0000

    moved mapping functions to SchemaStore

---
+++ SchemaStore/src/test/org/mitre/schemastore/mapfunctions/NumericAddTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2009 The MITRE Corporation (http://www.mitre.org/). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package test.org.mitre.schemastore.mapfunctions;
+
+import org.mitre.schemastore.mapfunctions.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class NumericAddTest {
+
+    private NumericAdd add = null;
+
+    /**
+     * Sets up the test fixture.
+     * (Called before every test case method.)
+     */
+    @Before
+    public void setUp() {
+        add = new NumericAdd();
+        add.addInput( 9 ); //Ht attribute
+        add.addInput( 10 ); //Wt attribute
+    }
+
+    /**
+     * Tears down the test fixture.
+     * (Called after every test case method.)
+     */
+    @After
+    public void tearDown() {
+        add = null;
+    }
+
+    @Test
+    public void testGetRelationalString() {
+        assertEquals("personID + firstName", add.getRelationalString("", TestFixtures.getSourceGraph()));
+    }
+}
+
