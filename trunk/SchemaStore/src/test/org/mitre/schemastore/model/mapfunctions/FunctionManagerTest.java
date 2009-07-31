0d7b2ef0f4a875974a4822d974ff2aeab4b3ff50
jchoyt <jchoyt@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-07-31 18:10:02 +0000

    moving mappingfunctions under model

---
+++ trunk/SchemaStore/src/test/org/mitre/schemastore/model/mapfunctions/FunctionManagerTest.java
@@ -0,0 +1,52 @@
+package test.org.mitre.schemastore.mapfunctions;
+
+import java.util.*;
+
+import org.mitre.schemastore.mapfunctions.*;
+import java.rmi.RemoteException;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class FunctionManagerTest {
+
+    //replace AbstractMappingFunction with the new fixture
+    private FunctionManager fixture = null;
+
+    /**
+     * Sets up the test fixture.
+     * (Called before every test case method.)
+     */
+    @Before
+    public void setUp() {
+        //add test fixtures for the mapping function here
+        fixture = new FunctionManager();
+    }
+
+    /**
+     * Tears down the test fixture.
+     * (Called after every test case method.)
+     */
+    @After
+    public void tearDown() {
+        fixture = null;
+    }
+
+    @Test
+    public void testParsing() {
+        List<String> functions = fixture.getAvailableFunctions();
+        assertFalse( "No functions seem to be avaiable", functions.size() == 0 );
+        assertTrue( "NumericAdd was not found", functions.contains( "Numeric Add" ));
+    }
+
+
+    @Test
+    public void testRetrieval() throws Exception
+    {
+        AbstractMappingFunction f = fixture.getFunction("Numeric Add");
+        assertNotNull( f );
+        assertTrue( f instanceof AbstractMappingFunction );
+        assertTrue( f instanceof org.mitre.schemastore.mapfunctions.NumericAdd );
+    }
+
+}
+
