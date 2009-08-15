241d959bee837eac61d8591ae46ac966032a0a4c
cwolf <cwolf@mitre.org>
2009-08-15 03:57:18 +0000

    Rename getGraphModel and setGraphModel to geetGraphModel and seetGraphModel

--- SchemaStoreClient/src/org/mitre/schemastore/porters/mappingExporters/PieChartExporter.java
+++ SchemaStoreClient/src/org/mitre/schemastore/porters/mappingExporters/PieChartExporter.java
@@ -83,7 +83,7 @@ public class PieChartExporter extends MappingExporter
 		int noCount = 0;
 		
 		// Cycles through all tree nodes to identify good, weak, and no links
-		HierarchicalGraph graph = new HierarchicalGraph(client.getGraph(schema.getId()),schema.getGraphModel());
+		HierarchicalGraph graph = new HierarchicalGraph(client.getGraph(schema.getId()),schema.geetGraphModel());
 		for(SchemaElement element : graph.getGraphElements())
 		{
 			double maxScore = Double.MIN_VALUE;
