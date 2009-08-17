5e6b6f3f89a638cdadd090f5e0fb7e544c3628ea
jkorb@mitre.org <jkorb@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-08-17 14:03:43 +0000

    Made it dynamic.

--- trunk/Harmony/src/org/mitre/harmony/view/dialogs/matcher/TypeDialog.java
+++ trunk/Harmony/src/org/mitre/harmony/view/dialogs/matcher/TypeDialog.java
@@ -2,11 +2,14 @@
 // ALL RIGHTS RESERVED
 package org.mitre.harmony.view.dialogs.matcher;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import java.awt.*;
+
+
 
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
@@ -17,14 +20,18 @@ import javax.swing.border.EmptyBorder;
 
 import org.mitre.harmony.matchers.mergers.TypedVoteMerger.TypeMappings;
 import org.mitre.harmony.model.HarmonyModel;
+import org.mitre.harmony.model.SchemaManager;
+import org.mitre.harmony.model.mapping.MappingManager;
 import org.mitre.harmony.view.dialogs.AbstractButtonPane;
 import org.mitre.schemastore.model.Attribute;
 import org.mitre.schemastore.model.Containment;
 import org.mitre.schemastore.model.Domain;
 import org.mitre.schemastore.model.DomainValue;
 import org.mitre.schemastore.model.Entity;
+import org.mitre.schemastore.model.MappingSchema;
 import org.mitre.schemastore.model.Relationship;
 import org.mitre.schemastore.model.SchemaElement;
+import org.mitre.schemastore.model.graph.HierarchicalGraph;
 
 /**
  * Class used for selecting which type(s) to match to which type(s)
@@ -33,6 +40,8 @@ import org.mitre.schemastore.model.SchemaElement;
  */
 class TypeDialog extends JDialog
 {		
+	private HarmonyModel harmonyModel;
+	
 	// List of selected match voters
 	private TypeMappings typeMappings = new TypeMappings();
 	
@@ -52,7 +61,7 @@ class TypeDialog extends JDialog
 			this.sourceType = sourceType;
 			this.targetType = targetType;
 			setText("");
-			setBackground(Color.white);
+//			setBackground(Color.white);
 			setFocusable(false);
 		}
 	}
@@ -83,46 +92,114 @@ class TypeDialog extends JDialog
 			{ dispose(); }
 	}
 	
+	
+	
+	
 	/** Generates the type dialog */ @SuppressWarnings("unchecked")
 	private JPanel mainPane()
 	{
+		// Generate the source and target labels
+		HashSet<SchemaElement> leftSchemaElements = harmonyModel.getMappingManager().getSchemaElements(MappingSchema.LEFT);
+//			harmonyModel.getSelectedInfo().getSelectedElements(MappingSchema.LEFT);
+		HashSet<SchemaElement> rightSchemaElements = harmonyModel.getMappingManager().getSchemaElements(MappingSchema.RIGHT);
+
+		SchemaManager sm = harmonyModel.getSchemaManager();
+//		String [] type1Array = { "Entity", "Attribute", "Domain", "DomainValue", "Relationship", "Containment"};
+		
+//		Class[] types = new Class[] { Entity.class, Attribute.class, Domain.class, DomainValue.class, Relationship.class, Containment.class };		
+		
+		HashMap<Class, Integer> sourceTypes = new HashMap<Class, Integer>();
+		for (SchemaElement e : leftSchemaElements) {
+			sourceTypes.put(e.getClass(), 1);
+		}
+		HashMap<Class, Integer> targetTypes = new HashMap<Class, Integer>();
+		for (SchemaElement e: rightSchemaElements) {
+			targetTypes.put(e.getClass(), 1);
+		}
+		
+		
+		
 		// Create pane for storing all checkbox items
 		JPanel checkboxPane = new JPanel();
-		checkboxPane.setBackground(Color.white);
-		checkboxPane.setLayout(new GridLayout(6, 6));
+//		checkboxPane.setBackground(Color.white);
+		checkboxPane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		// Generate the source label
+		JLabel sourceLabel = new JLabel("Source", JLabel.CENTER);
+		sourceLabel.setUI(new VerticalLabelUI(false));
+		sourceLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+		c.gridx = 0;
+		c.ipadx = 20;
+		c.fill = GridBagConstraints.VERTICAL;
+		c.gridheight = sourceTypes.size();
+		c.gridy = 2;
+		checkboxPane.add(sourceLabel, c);
+
+		c.ipadx = 0;
+		c.gridheight = 1;
+		
+		
+		// Generate the target label
+		JLabel targetLabel = new JLabel("Target", JLabel.CENTER);
+		targetLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+		c.ipady = 20; 
+		c.gridx = 2;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = targetTypes.size();
+		c.gridy = 0;
+		checkboxPane.add(targetLabel, c);
+		
+		c.gridwidth = 1;
+		c.ipady = 0; 
+		
+		int x = 2;
+		int y = 2;
+		for (Class type : sourceTypes.keySet()) {
+			JLabel sLabel = new JLabel(type.getSimpleName());
+//			sLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+			c.gridx = 1;
+			c.gridy = y++;
+			checkboxPane.add(sLabel, c);
+		}
+		for (Class type : targetTypes.keySet()) {
+			JLabel tLabel = new JLabel(type.getSimpleName());
+//			tLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+			tLabel.setUI(new VerticalLabelUI(false));
+			c.gridx = x++;
+			c.gridy = 1;
+			checkboxPane.add(tLabel, c);		
+		}
 		
 		// Generate the type grid
-		Class[] types = new Class[] { Entity.class, Attribute.class, Domain.class, DomainValue.class, Relationship.class, Containment.class };		
-		for(Class sourceType : types)
-			for(Class targetType : types)
+		x = 2;
+		y = 2;
+		
+		for(Class sourceType : sourceTypes.keySet()) {
+			for(Class targetType : targetTypes.keySet())
 			{
 				TypeCheckBox checkbox = new TypeCheckBox(sourceType, targetType);
 				checkboxes.add(checkbox);
 				if(sourceType.equals(targetType)) checkbox.setSelected(true);
-				checkboxPane.add(checkbox);
+				c.gridx = x++;
+				c.gridy = y;
+				checkboxPane.add(checkbox, c);
 			}
+			x = 2;
+			y++;
+		}
 		
-		JScrollPane checkboxScrollPane = new JScrollPane(checkboxPane,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		checkboxScrollPane.setPreferredSize(new Dimension(250, 300));
+//		JPanel checkboxPane = new JPanel(checkboxPane);
+//		checkboxPane.setPreferredSize(new Dimension(250, 300));
 	    
 		// Place list of roots in center of project pane
 		JPanel pane = new JPanel();
-		pane.setBorder(new EmptyBorder(20,20,20,20));
+//		pane.setBorder(new EmptyBorder(20,20,20,20));
 		pane.setLayout(new BorderLayout());
-		pane.add(new JLabel("                          Entity  Attribute Domain DomainValue Relationship Containment     "),BorderLayout.NORTH);
 		
-		JLabel longLabel = new JLabel();
-		longLabel.setText("<html><body>Entity<p><p>" +
-				"Attribute<p><p>" +
-				"Domain<p><p>" +
-				"DomainValue<p><p><p>" +
-				"Relationship <p><p>" +
-				"Containment<p><p>" +
-				"</body></html>");
 
-		pane.add(longLabel, BorderLayout.WEST);
 		
-		pane.add(checkboxScrollPane,BorderLayout.CENTER);
+		pane.add(checkboxPane,BorderLayout.CENTER);
 		pane.add(new ButtonPane(),BorderLayout.SOUTH);
 		return pane;
 	}
@@ -131,6 +208,9 @@ class TypeDialog extends JDialog
 	TypeDialog(HarmonyModel harmonyModel)
 	{
 		super(harmonyModel.getBaseFrame());
+		
+		this.harmonyModel = harmonyModel;
+		
 		setTitle("Which Types Would You Like To Match?");
 		setModal(true);
     	setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
