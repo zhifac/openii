c2647133c310e6c27fed19f60643d851f72f241d
jchoyt <jchoyt@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-09-04 15:49:04 +0000

    More functions!!

---
+++ SchemaStore/src/org/mitre/schemastore/model/mapfunctions/ExtractHour.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2009 The MITRE Corporation (http://www.mitre.org/). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.mitre.schemastore.model.mapfunctions;
+
+/**
+ *  This class provides the for the addition of two fields.
+ *  @author     Jeffrey Hoyt
+ *  @version    1.0
+ */
+public class ExtractHour extends ExtractYear
+{
+
+    public ExtractHour( )
+    {
+        super();
+        functionName = "EXTRACT(HOUR FROM ";
+        displayName = "Extract Hour";
+        description = "Extracts the hour of the day from a date field";
+        KEY = String.valueOf( getClass().getName() );
+    }
+
+}
+
---
+++ SchemaStore/src/org/mitre/schemastore/model/mapfunctions/LowerCase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2009 The MITRE Corporation (http://www.mitre.org/). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.mitre.schemastore.model.mapfunctions;
+
+import org.mitre.schemastore.model.mappingInfo.MappingInfo;
+import java.util.ArrayList;
+
+/**
+ *  This class provides the for the addition of two fields.
+ *  @author     Jeffrey Hoyt
+ *  @version    1.0
+ */
+public class LowerCase extends SingularFunction
+{
+
+    public LowerCase( )
+    {
+        super();
+        inputDomains=new ArrayList<String>();
+        inputDomains.add( MappingInfo.STRING );
+        functionName = "LOWER";
+        displayName = "Lower Case";
+        description = "Returns the value with all letters in lower case";
+        KEY = String.valueOf( getClass().getName() );
+    }
+
+}
+
