c2012c313dff7a7f89e67c0e665f0edb2b351f7a
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-09-23 14:29:01 +0000

    Fix TypeMappings to be more efficient

--- trunk/Harmony/src/org/mitre/harmony/matchers/TypeMappings.java
+++ trunk/Harmony/src/org/mitre/harmony/matchers/TypeMappings.java
@@ -1,37 +1,28 @@
 package org.mitre.harmony.matchers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 
 import org.mitre.schemastore.model.SchemaElement;
 
 /** Class for managing the mapping between types */
 public class TypeMappings
-{
+{	
 	/** Stores the mapping between types */
-	private HashMap<Class<SchemaElement>,ArrayList<Class<SchemaElement>>> mapping = new HashMap<Class<SchemaElement>,ArrayList<Class<SchemaElement>>>();
-
+	private HashSet<String> mapping = new HashSet<String>();
+	
+	/** Returns the key associated with the class pair */
+	private String getKey(Class type1, Class type2)
+		{ return type1.getSimpleName() + "_" + type2.getSimpleName(); }
+	
 	/** Adds the specified pair of types to the mapping */
 	public void addMapping(Class<SchemaElement> type1, Class<SchemaElement> type2)
-	{
-		ArrayList<Class<SchemaElement>> mappedTypes = mapping.get(type1);
-		if(mappedTypes==null) mapping.put(type1, mappedTypes = new ArrayList<Class<SchemaElement>>());
-		mappedTypes.add(type2);
-	}
+		{ mapping.add(getKey(type1,type2)); }
 	
 	/** Indicates if there is a mapping between the two specified classes */
 	public boolean isMapped(SchemaElement element1, SchemaElement element2)
-	{
-		ArrayList<Class<SchemaElement>> mappedTypes = mapping.get(element1.getClass());
-		return mappedTypes!=null && mappedTypes.contains(element2.getClass());
-	}
+		{ return mapping.contains(getKey(element1.getClass(),element2.getClass())); }
 	
 	/** Returns the number of type pairs which have been mapped */
 	public Integer size()
-	{
-		Integer size=0;
-		for(ArrayList<Class<SchemaElement>> mappedTypes : mapping.values())
-			size += mappedTypes.size();
-		return size;
-	}
+		{ return mapping.size(); }
 }
