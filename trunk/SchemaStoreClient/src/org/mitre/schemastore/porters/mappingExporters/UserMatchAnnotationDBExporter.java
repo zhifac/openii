f0f62892385791466400c853b8cac7f1e584a332
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2009-09-23 16:00:44 +0000

    Remove need to refer to null SchemaModel

--- trunk/SchemaStoreClient/src/org/mitre/schemastore/porters/mappingExporters/UserMatchAnnotationDBExporter.java
+++ trunk/SchemaStoreClient/src/org/mitre/schemastore/porters/mappingExporters/UserMatchAnnotationDBExporter.java
@@ -82,7 +82,7 @@ public class UserMatchAnnotationDBExporter  extends UserMatchAnotationExporter {
 			// Initialize the schema and schema element lists
 			elements = getSchemaElements(Arrays.asList(mapping.getSchemaIDs()));
 			for(MappingSchema schema : mapping.getSchemas())
-				schemaInfoList.add(new HierarchicalSchemaInfo(client.getSchemaInfo(schema.getId()),null));
+				schemaInfoList.add(new HierarchicalSchemaInfo(client.getSchemaInfo(schema.getId())));
 
 			// Get the list of mapping cells
 			CompressedList matchList = new CompressedList();
