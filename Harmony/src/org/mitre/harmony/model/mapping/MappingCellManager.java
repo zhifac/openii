fec6ca87e562537078b7fef9faaace335f67b093
cwolf <cwolf@mitre.org>
2009-12-08 20:32:14 +0000

    Fix MappingCellManager to be more side specific

--- Harmony/src/org/mitre/harmony/model/mapping/MappingCellManager.java
+++ Harmony/src/org/mitre/harmony/model/mapping/MappingCellManager.java
@@ -1,7 +1,9 @@
 package org.mitre.harmony.model.mapping;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -29,11 +31,29 @@ public class MappingCellManager extends AbstractManager<MappingCellListener> imp
 	/** Stores the mapping cells */
 	protected HashMap<Integer,MappingCell> mappingCellHash = new HashMap<Integer,MappingCell>();
 
+	/** Stores the mapping cells by key */
+	protected HashMap<String,Integer> mappingCellsByKey = new HashMap<String,Integer>();
+	
 	/** Stores the mapping cells by element reference */
-	protected HashMap<Integer,HashMap<Integer,Integer>> mappingCellsByElement = new HashMap<Integer,HashMap<Integer,Integer>>();
+	protected HashMap<Integer,ArrayList<Integer>> mappingCellsByElement = new HashMap<Integer,ArrayList<Integer>>();
 	
 	/** Stores the last mapping cell id handed out */
 	private Integer maxID = 0;
+	
+	/** Returns the key associated with the specified element IDs */
+	private String getKey(Integer[] inputIDs, Integer outputID)
+	{
+		// Sort the list of elements
+		List<Integer> sortedInputIDs = Arrays.asList(inputIDs);
+		Collections.sort(sortedInputIDs);
+
+		// Retrieve the mapping cell using the key
+		StringBuffer key = new StringBuffer();
+		for(Integer sortedInputID : sortedInputIDs)
+			key.append(sortedInputID + " ");
+		key.append(outputID);
+		return key.toString();
+	}
 	
 	/** Retrieve the mapping cells for the specified mapping cell IDs */
 	private List<MappingCell> getMappingCellsByID(List<Integer> mappingCellIDs)
@@ -51,27 +71,26 @@ public class MappingCellManager extends AbstractManager<MappingCellListener> imp
 	/** Returns a list of all mapping cells */
 	public ArrayList<MappingCell> getMappingCells()
 		{ return new ArrayList<MappingCell>(mappingCellHash.values()); }
-	
-	public boolean isMappingCellFinished(Integer linkID)
-		{ return false; }
 
-	/** Returns the requested mapping cell ID */
-	public Integer getMappingCellID(Integer element1ID, Integer element2ID)
-	{
-		HashMap<Integer,Integer> elementMappingCells = mappingCellsByElement.get(element1ID);
-		return elementMappingCells==null ? null : elementMappingCells.get(element2ID);
-	}
-
-	/** Returns a list of all mapping cells linked to the specified element */
-	public ArrayList<Integer> getMappingCellsByElement(Integer elementID)
-	{
-		if(mappingCellsByElement.get(elementID)==null) return new ArrayList<Integer>();
-		return new ArrayList<Integer>(mappingCellsByElement.get(elementID).values());
-	}
-	
 	/** Returns the requested mapping cell */
 	public MappingCell getMappingCell(Integer mappingCellID)
 		{ return mappingCellHash.get(mappingCellID).copy(); }
+	
+	/** Returns the requested mapping cell ID */
+	public Integer getMappingCellID(Integer inputID, Integer outputID)
+		{ return mappingCellsByKey.get(getKey(new Integer[] {inputID},outputID)); }
+
+	/** Returns the requested mapping cell ID */
+	public Integer getMappingCellID(Integer[] inputIDs, Integer outputID)
+		{ return mappingCellsByKey.get(getKey(inputIDs,outputID)); }	
+	
+	/** Returns a list of all mapping cells linked to the specified element */
+	public ArrayList<Integer> getMappingCellsByElement(Integer elementID)
+	{
+		ArrayList<Integer> mappingCellIDs = mappingCellsByElement.get(elementID);
+		if(mappingCellIDs==null) mappingCellIDs = new ArrayList<Integer>();
+		return mappingCellIDs;
+	}
 	
 	/** Sets the mapping cell */
 	public void setMappingCells(List<MappingCell> mappingCells)
@@ -92,17 +111,24 @@ public class MappingCellManager extends AbstractManager<MappingCellListener> imp
 			{
 				// Get information about the mapping cell to be stored
 				Integer mappingCellID = ++maxID;
-				Integer element1 = mappingCell.getElement1();
-				Integer element2 = mappingCell.getElement2();
+				Integer inputIDs[] = mappingCell.getInput();
+				Integer outputID = mappingCell.getOutput();
 				mappingCell.setId(mappingCellID);
 				mappingCell.setModificationDate(Calendar.getInstance().getTime());
 	
-				// Store the mapping cell info
+				// Store the mapping cell info in the hash and by key
 				mappingCellHash.put(mappingCellID, mappingCell);
-				if(mappingCellsByElement.get(element1)==null) mappingCellsByElement.put(element1,new HashMap<Integer,Integer>());
-				if(mappingCellsByElement.get(element2)==null) mappingCellsByElement.put(element2,new HashMap<Integer,Integer>());
-				mappingCellsByElement.get(element1).put(element2,mappingCellID);
-				mappingCellsByElement.get(element2).put(element1,mappingCellID);
+				mappingCellsByKey.put(getKey(inputIDs,outputID),mappingCellID);
+				
+				// Stores the mapping cell info be element reference
+				ArrayList<Integer> elementIDs = new ArrayList<Integer>(Arrays.asList(inputIDs));
+				elementIDs.add(outputID);
+				for(Integer elementID : elementIDs)
+				{
+					ArrayList<Integer> mappingCellIDs = mappingCellsByElement.get(elementID);
+					if(mappingCellIDs==null) mappingCellsByElement.put(elementID, mappingCellIDs = new ArrayList<Integer>());
+					mappingCellIDs.add(mappingCellID);
+				}
 			}
 			for(MappingCellListener listener : getListeners()) listener.mappingCellsAdded(newMappingCells);
 		}
@@ -162,9 +188,19 @@ public class MappingCellManager extends AbstractManager<MappingCellListener> imp
 	{
 		for(MappingCell mappingCell : mappingCells)
 		{
-			mappingCellsByElement.get(mappingCell.getElement1()).remove(mappingCell.getElement2());
-			mappingCellsByElement.get(mappingCell.getElement2()).remove(mappingCell.getElement1());
+			// Remove the mapping cell from the hash and key caches
 			mappingCellHash.remove(mappingCell.getId());
+			mappingCellsByKey.remove(getKey(mappingCell.getInput(),mappingCell.getOutput()));
+
+			// Remove the mapping cell from the element reference
+			ArrayList<Integer> elementIDs = new ArrayList<Integer>(Arrays.asList(mappingCell.getInput()));
+			elementIDs.add(mappingCell.getOutput());
+			for(Integer elementID : elementIDs)
+			{
+				ArrayList<Integer> mappingCellIDs = mappingCellsByElement.get(elementID);
+				if(mappingCellIDs.size()>1) mappingCellIDs.remove(mappingCell.getId());
+				else mappingCellsByElement.remove(elementID);
+			}
 		}
 		for(MappingCellListener listener : getListeners()) listener.mappingCellsRemoved(mappingCells);		
 	}
@@ -179,16 +215,21 @@ public class MappingCellManager extends AbstractManager<MappingCellListener> imp
 
 	/** Handles the removal of a schema from the mapping */
 	public void schemaRemoved(Integer schemaID)
-	{
+	{	
 		// Identify all elements associated with remaining schemas
-		HashSet<Integer> elementIDs = getModel().getMappingManager().getSchemaElementIDs(MappingSchema.LEFT);
-		elementIDs.addAll(getModel().getMappingManager().getSchemaElementIDs(MappingSchema.RIGHT));
+		HashSet<Integer> leftIDs = getModel().getMappingManager().getSchemaElementIDs(MappingSchema.LEFT);
+		HashSet<Integer> rightIDs = getModel().getMappingManager().getSchemaElementIDs(MappingSchema.RIGHT);
 
 		// Identify all mapping cells for which elements are no longer available
 		ArrayList<MappingCell> mappingCells = new ArrayList<MappingCell>();
 		for(MappingCell mappingCell : getMappingCells())
-			if(!elementIDs.contains(mappingCell.getElement1()) || !elementIDs.contains(mappingCell.getElement2()))
-				mappingCells.add(mappingCell);
+		{
+			boolean exists = true;
+			for(Integer inputID : mappingCell.getInput())
+				exists &= leftIDs.contains(inputID);
+			exists &= rightIDs.contains(mappingCell.getOutput());
+			if(!exists) mappingCells.add(mappingCell);
+		}				
 		deleteMappingCells(mappingCells);
 	}
 	
