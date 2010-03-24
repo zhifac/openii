129b38f570396b2d62bf55086ac4a8eebdbae9af
plockaby@mitre.org <plockaby@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-03-24 18:53:43 +0000

    removed unused import from the new AddRepositoryCommand.java updated RMapEditor.java to use new RMapGUI class

--- trunk/OpenII/src/org/mitre/openii/commands/AddRepositoryCommand.java
+++ trunk/OpenII/src/org/mitre/openii/commands/AddRepositoryCommand.java
@@ -3,7 +3,6 @@ package org.mitre.openii.commands;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.mitre.openii.model.EditorManager;
 import org.mitre.openii.views.repositories.EditRepositoryDialog;
 
 public class AddRepositoryCommand extends AbstractHandler {
--- trunk/OpenII/src/org/mitre/openii/editors/mappings/RMapEditor.java
+++ trunk/OpenII/src/org/mitre/openii/editors/mappings/RMapEditor.java
@@ -9,8 +9,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.mitre.openii.editors.OpenIIEditor;
 import org.mitre.openii.model.RepositoryManager;
-
-import GUI.SQLGeneratorGUI;
+import org.mitre.rmap.view.RMapGUI;
 
 /** Constructs the RMap View */
 public class RMapEditor extends OpenIIEditor
@@ -18,18 +17,17 @@ public class RMapEditor extends OpenIIEditor
 	/** Displays the RMap View */
 	public void createPartControl(Composite parent)
 	{
-		// Construct the applet pane to contain the Harmony frame
-		JApplet appletPane = new JApplet();
-		try {
-		appletPane.add(new SQLGeneratorGUI(RepositoryManager.getClient(), elementID));
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-		//	appletPane.add(new JLabel("some text"));
-		
-		// Embed the applet pane into the Eclipse view
-		Composite swtAwtComponent = new Composite(parent, SWT.EMBEDDED);
-		Frame frame = SWT_AWT.new_Frame( swtAwtComponent );
-		frame.add(appletPane);
+		// Construct the applet pane to contain the Harmony frame 
+	    JApplet appletPane = new JApplet(); 
+	    try { 
+	    	appletPane.add(new RMapGUI(RepositoryManager.getClient(), elementID)); 
+	    } catch (Exception e){ 
+            e.printStackTrace(); 
+	    } 
+
+	    // Embed the applet pane into the Eclipse view 
+	    Composite swtAwtComponent = new Composite(parent, SWT.EMBEDDED); 
+	    Frame frame = SWT_AWT.new_Frame( swtAwtComponent ); 
+    	frame.add(appletPane); 
 	}
 }
