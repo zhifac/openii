8870be42bc396771483a0207f73a111cfcac93e4
plockaby@mitre.org <plockaby@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-03-24 16:50:20 +0000

    Enable functionality from the menu bar and fix wording to match functionality.

--- trunk/OpenII/src/org/mitre/openii/commands/AboutOpenIICommand.java
+++ trunk/OpenII/src/org/mitre/openii/commands/AboutOpenIICommand.java
@@ -5,10 +5,10 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.mitre.openii.model.EditorManager;
 
-/** Class for implementing the AboutOpenII command */
-public class AboutOpenIICommand extends AbstractHandler
-{
+public class AboutOpenIICommand extends AbstractHandler {
 	/** Executes the AboutOpenII command by displaying the AboutOpenII Editor */
-	public Object execute(ExecutionEvent event) throws ExecutionException
-		{ EditorManager.launchEditor("AboutOpenIIEditor", null); return null; }
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		EditorManager.launchEditor("AboutOpenIIEditor", null);
+		return null;
+	}
 }
