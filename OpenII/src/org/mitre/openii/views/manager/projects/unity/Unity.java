95797dd8ada49d6563e8f8b9549ae00008f42024
haoli <haoli@mitre.org>
2010-03-28 19:52:07 +0000

    update unity

--- OpenII/src/org/mitre/openii/views/manager/projects/unity/Unity.java
+++ OpenII/src/org/mitre/openii/views/manager/projects/unity/Unity.java
@@ -1,25 +1,32 @@
 package org.mitre.openii.views.manager.projects.unity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
 
 import org.mitre.harmony.matchers.voters.MatchVoter;
+import org.mitre.openii.model.OpenIIManager;
 import org.mitre.openii.model.RepositoryManager;
+import org.mitre.openii.views.manager.projects.matchmaker.ClusterNode;
 import org.mitre.openii.views.manager.projects.matchmaker.MappingProcessor;
 import org.mitre.openii.views.manager.projects.matchmaker.MatchMaker;
 import org.mitre.openii.views.manager.projects.matchmaker.Synset;
 import org.mitre.openii.views.manager.projects.matchmaker.Term;
+import org.mitre.schemastore.model.Mapping;
+import org.mitre.schemastore.model.MappingCell;
 import org.mitre.schemastore.model.Project;
+import org.mitre.schemastore.model.ProjectSchema;
 import org.mitre.schemastore.model.Schema;
 import org.mitre.schemastore.model.SchemaElement;
 
 public class Unity {
 
 	private Project project;
-	private Vocabulary vocabulary; 
-	
+	private Vocabulary vocabulary;
+
 	public Unity(Project project) {
 		this.project = project;
-		this.vocabulary = new Vocabulary(project);
 	}
 
 	/**
@@ -41,40 +48,74 @@ public class Unity {
 	 * @throws Exception
 	 */
 	public Vocabulary generateVocabulary(ArrayList<MatchVoter> voters, String name, String author, String description) throws Exception {
-		Integer vocabularyID = null;
+		vocabulary = new Vocabulary(project);
 
 		// Ensure that all pair-wise mappings already exist
 		MappingProcessor.run(project, voters);
-
 		// Run MatchMaker to cluster the matches get synsets
-		vocabulary.setSynsetList( new MatchMaker(project).cluster().getSynsets() );
+		ClusterNode cluster  = new MatchMaker(project).cluster();
 
-		// Create a core schema and add an empty
-		vocabulary.setCore(  new Schema(0, name, author, null, null, description, false) );
+		// Set vocabulary synset
+		vocabulary.setSynsetList(cluster.getSynsets());
+		
+		// Create a vocabulary schema
+		Schema vocabSchema = new Schema(null, name, author, null, null, description, false); 
+		Integer vocabularyID = OpenIIManager.addSchema(vocabSchema);
+		vocabSchema.setId(vocabularyID);
+		vocabulary.setCore(vocabSchema);
 
-		vocabularyID = RepositoryManager.getClient().addSchema(vocabulary.getCore());
-		for (Synset s : vocabulary.getSynsetList()) {
-			SchemaElement newElement = new SchemaElement(); 
-			newElement.setName(createCoreTerm(s)); 
-			newElement.setBase(vocabularyID); 
-			Integer newElementID = RepositoryManager.getClient().addSchemaElement(newElement);
-			newElement.setId(newElementID); 
-			s.combineSynsets(new Synset(new Term(vocabularyID, newElementID, newElement.getName()))); 
+		// Add vocabulary schema to the project
+		Integer[] sourceSchemaIds = project.getSchemaIDs();
+		ArrayList<ProjectSchema> projSchemas = new ArrayList<ProjectSchema>();
+		for (Integer schemaId : sourceSchemaIds) {
+			projSchemas.add(new ProjectSchema(schemaId, OpenIIManager.getSchema(schemaId).getName(), null));
+		}
+		projSchemas.add( new ProjectSchema(vocabSchema.getId(), name, null) ); 
+		// TODO how to set a schema as a project vocabulary? 
+		
+		HashMap<Integer, Integer> mappingHash = new HashMap<Integer, Integer>();
+		// Add mappings for each source schema to the vocabulary schema
+		for (Integer sourceSchemaId : sourceSchemaIds) {
+			mappingHash.put(sourceSchemaId,OpenIIManager.addMapping(new Mapping(null, project.getId(), sourceSchemaId, vocabulary.getId())));
 		}
 
+		// Create a new term for each synset and mapping to existing terms
+		for (Synset synset : vocabulary.getSynsetList()) {
+			// Create a new schema element for the new term in schemastore
+			SchemaElement newElement = new SchemaElement();
+			newElement.setName(createCoreTerm(synset));
+			newElement.setBase(vocabulary.getId());
+			
+			Integer newElementID = RepositoryManager.getClient().addSchemaElement(newElement);
+			newElement.setId(newElementID);
+			
+			// Create mapping cells between the new term and the other term in the synset
+			ArrayList<Term> sourceTerm = synset.getGroup(); 
+			for ( Term term : sourceTerm ) {
+				MappingCell.createValidatedMappingCell(null, mappingHash.get(term.schemaId), Arrays.asList(new Integer(term.elementId)).toArray(new Integer[0])	, newElementID, "unity", new Date(), "map function?", "");
+			}
+
+			// Add new term to synset
+			synset.combineSynsets(new Synset(new Term(vocabulary.getId(), newElementID, newElement.getName())));
+
+		}
 		return vocabulary;
 	}
 
 	/**
 	 * TODO currently returning some random schema name.
-	 * @param s
+	 * 
+	 * @param synset
 	 * @return
 	 */
-	private String createCoreTerm(Synset s) {
-		return s.getGroup().get(0).elementName; 
+	protected String createCoreTerm(Synset synset) {
+		for (Term term : synset.getGroup())
+			if (term.schemaId.equals(vocabulary.getId())) return term.elementName;
+
+		return null;
 	}
-	
-	public Vocabulary getVocabulary(){
+
+	public Vocabulary getVocabulary() {
 		return vocabulary;
 	}
 
--- OpenII/src/org/mitre/openii/views/manager/projects/unity/Vocabulary.java
+++ OpenII/src/org/mitre/openii/views/manager/projects/unity/Vocabulary.java
@@ -2,32 +2,43 @@ package org.mitre.openii.views.manager.projects.unity;
 
 import java.util.ArrayList;
 
+import org.mitre.openii.model.OpenIIManager;
 import org.mitre.openii.views.manager.projects.matchmaker.Synset;
 import org.mitre.schemastore.model.Project;
 import org.mitre.schemastore.model.Schema;
+import org.mitre.schemastore.model.schemaInfo.SchemaInfo;
 
-public class Vocabulary {
-	private Schema core = null;
+public class Vocabulary  {
+
+	private SchemaInfo core = null;
 	private ArrayList<Synset> synsetList = null;
 
-	public Vocabulary(Project project) {
-	// TODO Given a project from the database, get all of the schema, schema
-	// elements, core schema, and recreate the synsets
-	}
+	 public Vocabulary(Project project) {
+	 // TODO Given a project from the database, get all of the schema, schema
+	 // elements, core schema, and recreate the synsets
+	 }
 
-	public Schema getCore() {
+	public SchemaInfo getCore() {
 		return core;
 	}
 
+	/**
+	 * From existing project, generate the synsets. 
+	 * @return
+	 */
 	public ArrayList<Synset> getSynsetList() {
 		return synsetList;
 	}
 
 	public void setCore(Schema core) {
-		this.core = core;
+		this.core = OpenIIManager.getSchemaInfo(core.getId());
 	}
 
 	public void setSynsetList(ArrayList<Synset> synsetList) {
 		this.synsetList = synsetList;
+	}
+	
+	public Integer getId() {
+		return core.getSchema().getId(); 
 	}
 }
