953f38746cd8da0fd99f8d8015baba46f24565ca
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-04-01 16:50:42 +0000

    Replace old Mapping wording with "Project"

--- trunk/Harmony/src/org/mitre/harmony/view/harmonyPane/HarmonyMenuBar.java
+++ trunk/Harmony/src/org/mitre/harmony/view/harmonyPane/HarmonyMenuBar.java
@@ -172,8 +172,8 @@ public class HarmonyMenuBar extends JMenuBar
     		int option = 1;
     		if(harmonyModel.getProjectManager().isModified())
         		option = JOptionPane.showConfirmDialog(harmonyModel.getBaseFrame(),
-        			"This mapping has been modified.  Do you want to save changes?",
-					"Save Mapping", JOptionPane.YES_NO_CANCEL_OPTION,
+        			"This project has been modified.  Do you want to save changes?",
+					"Save Project", JOptionPane.YES_NO_CANCEL_OPTION,
 					JOptionPane.WARNING_MESSAGE);
     		if(option==2) return false;
     		if(option==0) new SaveMappingDialog(harmonyModel);
