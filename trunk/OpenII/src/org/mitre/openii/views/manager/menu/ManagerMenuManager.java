a48e6feb699521dcceaff9f57fcebbe152b0c8de
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-05-13 12:47:36 +0000

    Add in ability to export files in bulk

--- trunk/OpenII/src/org/mitre/openii/views/manager/menu/ManagerMenuManager.java
+++ trunk/OpenII/src/org/mitre/openii/views/manager/menu/ManagerMenuManager.java
@@ -15,6 +15,7 @@ import org.mitre.openii.model.OpenIIManager;
 import org.mitre.openii.views.manager.ManagerView;
 import org.mitre.openii.views.manager.SchemaInProject;
 import org.mitre.openii.views.manager.SchemaInTag;
+import org.mitre.openii.views.manager.menu.ManagerAction.ActionType;
 import org.mitre.schemastore.model.DataSource;
 import org.mitre.schemastore.model.Mapping;
 import org.mitre.schemastore.model.Project;
@@ -88,25 +89,25 @@ public class ManagerMenuManager extends MenuManager implements IMenuListener
 		// Display the menu for the "Schemas" header
 		if(element.equals(ManagerView.SCHEMAS_HEADER))
 		{
-			menuManager.add(new ManagerAction(this,"Import Schema",ManagerAction.IMPORT_SCHEMA));
-			menuManager.add(new ManagerAction(this,"New Tag",ManagerAction.NEW_TAG));
+			menuManager.add(new ManagerAction(this,"Import Schema",ActionType.IMPORT_SCHEMA));
+			menuManager.add(new ManagerAction(this,"New Tag",ActionType.NEW_TAG));
 		}
 		
 		// Display the menu for a selected schema
 		if(element instanceof Schema)
 		{
 			// Display schema options
-			menuManager.add(new ManagerAction(this,"Edit Schema Properties",ManagerAction.EDIT_SCHEMA));
-			menuManager.add(new ManagerAction(this,"Extend Schema",ManagerAction.EXTEND_SCHEMA));
-			menuManager.add(new ManagerAction(this,"Export Schema",ManagerAction.EXPORT_SCHEMA));
-			Action deleteAction = new ManagerAction(this,"Delete Schema",ManagerAction.DELETE_SCHEMA);
+			menuManager.add(new ManagerAction(this,"Edit Schema Properties",ActionType.EDIT_SCHEMA));
+			menuManager.add(new ManagerAction(this,"Extend Schema",ActionType.EXTEND_SCHEMA));
+			menuManager.add(new ManagerAction(this,"Export Schema",ActionType.EXPORT_SCHEMA));
+			Action deleteAction = new ManagerAction(this,"Delete Schema",ActionType.DELETE_SCHEMA);
 			if(!OpenIIManager.isDeletable(((Schema)element).getId())) deleteAction.setEnabled(false);
 			menuManager.add(deleteAction);
 
 			menuManager.add(new Separator());
 			
 			// Display option to create instance database
-			Action createDataSourceAction = new ManagerAction(this,"Create Instance Database",ManagerAction.CREATE_DATA_SOURCE);
+			Action createDataSourceAction = new ManagerAction(this,"Create Instance Database",ActionType.CREATE_DATA_SOURCE);
 			if(!((((Schema)element).getType()).equals("Spreadsheet Importer"))) createDataSourceAction.setEnabled(false);
 			menuManager.add(createDataSourceAction);
 		}
@@ -115,56 +116,57 @@ public class ManagerMenuManager extends MenuManager implements IMenuListener
 		if(element instanceof Tag)
 		{
 			// Display tag options
-			menuManager.add(new ManagerAction(this,"Add Subcategory",ManagerAction.NEW_TAG));
-			menuManager.add(new ManagerAction(this,"Edit Tag",ManagerAction.EDIT_TAG));
-			menuManager.add(new ManagerAction(this,"Delete Tag",ManagerAction.DELETE_TAG));
+			menuManager.add(new ManagerAction(this,"Add Subcategory",ActionType.NEW_TAG));
+			menuManager.add(new ManagerAction(this,"Edit Tag",ActionType.EDIT_TAG));
+			menuManager.add(new ManagerAction(this,"Delete Tag",ActionType.DELETE_TAG));
 	
 			menuManager.add(new Separator());
-
+			
 			// Display option to create project
-			menuManager.add(new ManagerAction(this,"Create Project",ManagerAction.CREATE_PROJECT_FROM_TAG));
+			menuManager.add(new ManagerAction(this,"Create Project",ActionType.CREATE_PROJECT_FROM_TAG));
+			menuManager.add(new ManagerAction(this,"Export Schemas",ActionType.EXPORT_SCHEMAS_BY_TAG));
 		}
 		
 		// Display the menu for a selection tag schema
 		if(element instanceof SchemaInTag)
-			menuManager.add(new ManagerAction(this,"Remove Schema from Tag",ManagerAction.DELETE_TAG_SCHEMA));
+			menuManager.add(new ManagerAction(this,"Remove Schema from Tag",ActionType.DELETE_TAG_SCHEMA));
 			
 		// Display the menu for the "Projects" header
 		if(element.equals(ManagerView.PROJECTS_HEADER))
 		{
-			menuManager.add(new ManagerAction(this,"New Project",ManagerAction.NEW_PROJECT));
-			menuManager.add(new ManagerAction(this,"Import Project",ManagerAction.IMPORT_PROJECT));
-			menuManager.add(new ManagerAction(this,"Merge Projects",ManagerAction.MERGE_PROJECTS));
+			menuManager.add(new ManagerAction(this,"New Project",ActionType.NEW_PROJECT));
+			menuManager.add(new ManagerAction(this,"Import Project",ActionType.IMPORT_PROJECT));
+			menuManager.add(new ManagerAction(this,"Merge Projects",ActionType.MERGE_PROJECTS));
 		}
 
 		// Display the menu for a selected project
 		if(element instanceof Project)
 		{
-			menuManager.add(new ManagerAction(this,"Edit Project",ManagerAction.EDIT_PROJECT));
-			menuManager.add(new ManagerAction(this,"Export Project",ManagerAction.EXPORT_PROJECT));
-			menuManager.add(new ManagerAction(this,"Delete Project",ManagerAction.DELETE_PROJECT));
+			menuManager.add(new ManagerAction(this,"Edit Project",ActionType.EDIT_PROJECT));
+			menuManager.add(new ManagerAction(this,"Export Project",ActionType.EXPORT_PROJECT));
+			menuManager.add(new ManagerAction(this,"Delete Project",ActionType.DELETE_PROJECT));
 			menuManager.add(new Separator());
-			menuManager.add(new ManagerAction(this,"Import Mapping",ManagerAction.IMPORT_MAPPING));
-			menuManager.add(new ManagerAction(this,"Generate Vocabulary...",ManagerAction.GENERATE_VOCABULARY));
+			menuManager.add(new ManagerAction(this,"Import Mapping",ActionType.IMPORT_MAPPING));
+			menuManager.add(new ManagerAction(this,"Generate Vocabulary...",ActionType.GENERATE_VOCABULARY));
 		}
 		
 		// Display the menu for a selected mapping
 		if(element instanceof Mapping)
 		{
-			menuManager.add(new ManagerAction(this,"Export Mapping",ManagerAction.EXPORT_MAPPING));
-			menuManager.add(new ManagerAction(this,"Delete Mapping",ManagerAction.DELETE_MAPPING));
+			menuManager.add(new ManagerAction(this,"Export Mapping",ActionType.EXPORT_MAPPING));
+			menuManager.add(new ManagerAction(this,"Delete Mapping",ActionType.DELETE_MAPPING));
 		}
 		
 		// Display the menu for a selection project schema
 		if(element instanceof SchemaInProject)
 		{
 			if(((SchemaInProject)element).isDeletable())
-				menuManager.add(new ManagerAction(this,"Remove Schema from Project",ManagerAction.DELETE_PROJECT_SCHEMA));
+				menuManager.add(new ManagerAction(this,"Remove Schema from Project",ActionType.DELETE_PROJECT_SCHEMA));
 		}
 		
 		// Display the menu for a selected data source
 		if(element instanceof DataSource)
-			menuManager.add(new ManagerAction(this,"Delete Data Source",ManagerAction.DELETE_DATA_SOURCE));
+			menuManager.add(new ManagerAction(this,"Delete Data Source",ActionType.DELETE_DATA_SOURCE));
 	}
 	
 	/** Generate the context menu before being displayed */
