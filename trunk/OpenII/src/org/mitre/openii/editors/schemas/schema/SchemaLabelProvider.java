efd71298cedcfa6d7ba2fcbe02bc1dfea37088b6
cwolf@mitre.org <cwolf@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-06-04 04:11:06 +0000

    Push in Relationship icon for Schema viewer

--- trunk/OpenII/src/org/mitre/openii/editors/schemas/schema/SchemaLabelProvider.java
+++ trunk/OpenII/src/org/mitre/openii/editors/schemas/schema/SchemaLabelProvider.java
@@ -7,6 +7,7 @@ import org.mitre.openii.application.OpenIIActivator;
 import org.mitre.schemastore.model.Attribute;
 import org.mitre.schemastore.model.Containment;
 import org.mitre.schemastore.model.DomainValue;
+import org.mitre.schemastore.model.Relationship;
 import org.mitre.schemastore.model.Schema;
 import org.mitre.schemastore.model.SchemaElement;
 import org.mitre.schemastore.model.schemaInfo.HierarchicalSchemaInfo;
@@ -28,6 +29,7 @@ public class SchemaLabelProvider implements ILabelProvider
 		else if(element instanceof DomainValue) imageName = "DomainValue.jpg";
 		else if(element instanceof Attribute) imageName = "Attribute.jpg";
 		else if(element instanceof Containment) imageName = "Containment.jpg";
+		else if(element instanceof Relationship) imageName = "Relationship.jpg";
 		else imageName = "SchemaElement.jpg";
 		return OpenIIActivator.getImage(imageName);
 	}
