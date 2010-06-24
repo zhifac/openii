fc8fb304844729f8c17825651f1f6cdef66d7170
plockaby@mitre.org <plockaby@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-06-24 17:41:19 +0000

    code cleaning before i start actually working with the wizard

--- trunk/Harmony/src/org/mitre/harmony/view/dialogs/matcher/wizard/Wizard.java
+++ trunk/Harmony/src/org/mitre/harmony/view/dialogs/matcher/wizard/Wizard.java
@@ -16,13 +16,12 @@ import javax.swing.JSeparator;
 import javax.swing.border.EmptyBorder;
 
 /** Class defining the wizard */
-public class Wizard extends WindowAdapter implements PropertyChangeListener
-{
+public class Wizard extends WindowAdapter implements PropertyChangeListener {
 	// Define the wizard return codes
 	public static final int FINISH_RETURN_CODE = 0;
     public static final int CANCEL_RETURN_CODE = 1;
     public static final int ERROR_RETURN_CODE = 2;
-        
+
     // Define the wizard button command strings
     public static final String NEXT_BUTTON_ACTION_COMMAND = "NextButtonActionCommand";
     public static final String BACK_BUTTON_ACTION_COMMAND = "BackButtonActionCommand";
@@ -32,21 +31,20 @@ public class Wizard extends WindowAdapter implements PropertyChangeListener
     private WizardModel wizardModel;
     private WizardController wizardController;
     private JDialog wizardDialog;
-        
+
     /** Store reference to the panel for showing the wizard panel */
     private JPanel cardPanel;
-    
+
     // References to the various wizard buttons
     private JButton backButton = new JButton();
     private JButton nextButton = new JButton();
     private JButton cancelButton = new JButton();
-    
+
     /** Stores the wizard return code */
     private int returnCode;
 
     /** Generates a button with the specified action command */
-    private JPanel generateButton(JButton button, String command)
-    {
+    private JPanel generateButton(JButton button, String command) {
     	// Generate the button
      	button.setActionCommand(command);
     	button.addActionListener(wizardController);
@@ -59,27 +57,26 @@ public class Wizard extends WindowAdapter implements PropertyChangeListener
     	pane.add(button,BorderLayout.CENTER);
     	return pane; 
     }
-    
+
     /** Constructs the wizard */
-    public Wizard(Frame owner)
-    {
+    public Wizard(Frame owner) {
     	// Initialize the wizard model and controller
         wizardController = new WizardController(this);       
     	wizardModel = new WizardModel();
         wizardModel.addPropertyChangeListener(this);       
-        
+
         // Generate the pane for displaying the wizard panel
         cardPanel = new JPanel();
         cardPanel.setBorder(new EmptyBorder(new Insets(5, 10, 5, 10)));       
         cardPanel.setLayout(new CardLayout());
-        
+
         // Generate the button box
         JPanel buttonPane = new JPanel();
         buttonPane.setLayout(new GridLayout(1,3));
         buttonPane.add(generateButton(backButton, BACK_BUTTON_ACTION_COMMAND));
         buttonPane.add(generateButton(nextButton, NEXT_BUTTON_ACTION_COMMAND));
         buttonPane.add(generateButton(cancelButton, CANCEL_BUTTON_ACTION_COMMAND));
-        
+
         // Generate the button pane
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BorderLayout());
@@ -95,8 +92,7 @@ public class Wizard extends WindowAdapter implements PropertyChangeListener
     }
 
     /** Shows the wizard dialog */
-    public int showDialog()
-    {    
+    public int showDialog() {    
         wizardDialog.setModal(true);
         wizardDialog.pack();
         wizardDialog.setLocationRelativeTo(getDialog().getParent());
@@ -105,83 +101,90 @@ public class Wizard extends WindowAdapter implements PropertyChangeListener
     }
 
     /** Returns the wizard dialog */
-    public JDialog getDialog()
-    	{ return wizardDialog; }
-    
+    public JDialog getDialog() {
+    	return wizardDialog;
+    }
+
     /** Returns the wizard model */
-    public WizardModel getModel()
-    	{ return wizardModel; }
-    
+    public WizardModel getModel() {
+    	return wizardModel;
+    }
+
     /** Registers the specified panel */
-    public void registerWizardPanel(String id, WizardPanel panel)
-    {
+    public void registerWizardPanel(String id, WizardPanel panel) {
         cardPanel.add(panel.getPanel(), id);
         panel.setWizard(this);
         wizardModel.registerPanel(id, panel);        
-    }  
-    
+    }
+
     /** Retrieves the specified panel */
-    public WizardPanel getPanel(Object id)
-    	{ return wizardModel.getPanel(id); }
-    
+    public WizardPanel getPanel(Object id) {
+    	return wizardModel.getPanel(id);
+    }
+
     /** Sets the currently displayed panel */
-    public void setCurrentPanel(String id)
-    {
+    public void setCurrentPanel(String id) {
     	// Don't proceed if a bad id was given
-    	if (id == null)
+    	if (id == null) {
             close(ERROR_RETURN_CODE);
-        
+    	}
+
     	// Informs old panel that it is about to be hidden
         WizardPanel oldPanel = wizardModel.getCurrentPanel();
-        if (oldPanel != null) oldPanel.aboutToHidePanel();
-        
+        if (oldPanel != null) {
+        	oldPanel.aboutToHidePanel();
+        }
+
         // Informs the new panel that it is about to be displayed
         wizardModel.setCurrentPanel(id);
         wizardModel.getCurrentPanel().aboutToDisplayPanel();
-        
+
         // Informs the new panel that it is being displayed
         ((CardLayout)cardPanel.getLayout()).show(cardPanel, id.toString());
         wizardModel.getCurrentPanel().displayingPanel();                
     }
 
     /** Handles a change to a wizard property */
-    public void propertyChange(PropertyChangeEvent evt)
-    {    
-        if (evt.getPropertyName().equals(WizardModel.CURRENT_PANEL_DESCRIPTOR_PROPERTY))
+    public void propertyChange(PropertyChangeEvent evt) {    
+        if (evt.getPropertyName().equals(WizardModel.CURRENT_PANEL_DESCRIPTOR_PROPERTY)) {
             wizardController.resetButtonsToPanelRules(); 
-        else if (evt.getPropertyName().equals(WizardModel.NEXT_BUTTON_TEXT_PROPERTY))
+        } else if (evt.getPropertyName().equals(WizardModel.NEXT_BUTTON_TEXT_PROPERTY)) {
             nextButton.setText(evt.getNewValue().toString());
-        else if (evt.getPropertyName().equals(WizardModel.BACK_BUTTON_TEXT_PROPERTY))            
+        } else if (evt.getPropertyName().equals(WizardModel.BACK_BUTTON_TEXT_PROPERTY)) {            
             backButton.setText(evt.getNewValue().toString());
-        else if (evt.getPropertyName().equals(WizardModel.CANCEL_BUTTON_TEXT_PROPERTY))      
+        } else if (evt.getPropertyName().equals(WizardModel.CANCEL_BUTTON_TEXT_PROPERTY)) {      
             cancelButton.setText(evt.getNewValue().toString());
-        else if (evt.getPropertyName().equals(WizardModel.NEXT_BUTTON_ENABLED_PROPERTY))
+        } else if (evt.getPropertyName().equals(WizardModel.NEXT_BUTTON_ENABLED_PROPERTY)) {
             nextButton.setEnabled(((Boolean)evt.getNewValue()).booleanValue());
-        else if (evt.getPropertyName().equals(WizardModel.BACK_BUTTON_ENABLED_PROPERTY))
+        } else if (evt.getPropertyName().equals(WizardModel.BACK_BUTTON_ENABLED_PROPERTY)) {
             backButton.setEnabled(((Boolean)evt.getNewValue()).booleanValue());
-        else if (evt.getPropertyName().equals(WizardModel.CANCEL_BUTTON_ENABLED_PROPERTY))            
+        } else if (evt.getPropertyName().equals(WizardModel.CANCEL_BUTTON_ENABLED_PROPERTY)) {            
             cancelButton.setEnabled(((Boolean)evt.getNewValue()).booleanValue());
+        }
     }
-    
+
     /** Returns the return code associated with the wizard */
-    public int getReturnCode()
-    	{ return returnCode; }
+    public int getReturnCode() {
+    	return returnCode;
+    }
 
     /** Sets the back button as enabled/disabled */
-    public void setBackButtonEnabled(boolean newValue)
-    	{ wizardModel.setProperty(WizardModel.BACK_BUTTON_ENABLED_PROPERTY,newValue); }
+    public void setBackButtonEnabled(boolean newValue) {
+    	wizardModel.setProperty(WizardModel.BACK_BUTTON_ENABLED_PROPERTY, newValue);
+    }
 
     /** Sets the next button as enabled/disabled */
-    public void setNextFinishButtonEnabled(boolean newValue)
-    	{ wizardModel.setProperty(WizardModel.NEXT_BUTTON_ENABLED_PROPERTY,newValue); }
-    
+    public void setNextFinishButtonEnabled(boolean newValue) {
+    	wizardModel.setProperty(WizardModel.NEXT_BUTTON_ENABLED_PROPERTY, newValue);
+    }
+
     /* Sets the cancel button as enabled/disabled */
-    public void setCancelButtonEnabled(boolean newValue)
-    	{ wizardModel.setProperty(WizardModel.CANCEL_BUTTON_ENABLED_PROPERTY,newValue); }
-    
+    public void setCancelButtonEnabled(boolean newValue) {
+    	wizardModel.setProperty(WizardModel.CANCEL_BUTTON_ENABLED_PROPERTY, newValue);
+    }
+
     /** Handles the closing of the wizard */
-    void close(int code)
-    {
+    void close(int code) {
         wizardModel.getCurrentPanel().aboutToHidePanel();
         returnCode = code;
         wizardDialog.dispose();
