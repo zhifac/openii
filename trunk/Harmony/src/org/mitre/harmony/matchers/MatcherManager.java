40b3951ef84deca99e830dcb5ee6a2f77d2f8e3a
plockaby@mitre.org <plockaby@mitre.org@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-06-28 15:12:30 +0000

    Use an actual XML parser to read the matchers.xml file. Added "options" to the matchers.xml file.

--- trunk/Harmony/src/org/mitre/harmony/matchers/MatcherManager.java
+++ trunk/Harmony/src/org/mitre/harmony/matchers/MatcherManager.java
@@ -2,29 +2,24 @@
 // ALL RIGHTS RESERVED
 package org.mitre.harmony.matchers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.mitre.harmony.matchers.mergers.MatchMerger;
 import org.mitre.harmony.matchers.voters.MatchVoter;
 import org.mitre.schemastore.model.schemaInfo.FilteredSchemaInfo;
 import org.mitre.schemastore.porters.PorterList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * Manages the Harmony Matchers
  * @author CWOLF
  */
-public class MatcherManager
-{
-	// Patterns used to extract voter and merger information
-	static private Pattern voterPattern = Pattern.compile("<voter( .*?)?>(.*?)</voter>");
-	static private Pattern mergerPattern = Pattern.compile("<merger>(.*?)</merger>");
-	
+public class MatcherManager {
 	/** Stores a listing of all match voters */
 	static private ArrayList<MatchVoter> voters = new ArrayList<MatchVoter>();
 
@@ -32,78 +27,113 @@ public class MatcherManager
 	static private ArrayList<MatchMerger> mergers = new ArrayList<MatchMerger>();
 	
 	/** Initializes the matcher manager with all defined match voters and mergers */
-	static
-	{
-		// Retrieve matcher configuration file
-		StringBuffer buffer = new StringBuffer("");
+	static {
 		try {
-			InputStream configStream = PorterList.class.getResourceAsStream("/matchers.xml");
-			BufferedReader in = new BufferedReader(new InputStreamReader(configStream));
-			String line; while((line=in.readLine())!=null) buffer.append(line);
-			in.close();
-		}
-		catch(IOException e)
-			{ System.out.println("(E)PorterManager - porters.xml has failed to load!\n"+e.getMessage()); }
-		
-		// Retrieve the match voters
-		Matcher voterMatcher = voterPattern.matcher(buffer);
-		while(voterMatcher.find())
-			try {
-				Class voterClass = Class.forName(voterMatcher.group(2));
-				MatchVoter voter = (MatchVoter)voterClass.newInstance();
-				String attrs = voterMatcher.group(1);
-				if(attrs!=null && attrs.contains("default='true'")) voter.setDefault(true);
-				if(attrs!=null && attrs.contains("hidden='true'")) voter.setHidden(true);
-				voters.add(voter);
-			}
-		    catch(Exception e) { System.out.println("(E)MatcherManager - Failed to locate voter class "+voterMatcher.group(1)); }
+			// using factory get an instance of document builder
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
 
-		// Retrieve match mergers
-		Matcher mergerMatcher = mergerPattern.matcher(buffer);
-		while(mergerMatcher.find())
-			try {
-				Class mergerClass = Class.forName(mergerMatcher.group(1));
-				mergers.add((MatchMerger)mergerClass.newInstance());
+			// parse using builder to get dom representation of the xml file
+			Document dom = db.parse(PorterList.class.getResourceAsStream("/matchers.xml"));
+
+			// get the root element
+			Element root = dom.getDocumentElement();
+
+			// get all voters
+			{
+				NodeList nodes = root.getElementsByTagName("voter");
+				if (nodes != null && nodes.getLength() > 0) {
+					for (int i = 0; i < nodes.getLength(); i++) {
+						// get the element and its attribute
+						Element node = (Element)nodes.item(i);
+
+						try {
+							// create a new voter from this class id
+							Class voterClass = Class.forName(node.getAttribute("id"));
+							MatchVoter voter = (MatchVoter)voterClass.newInstance();
+	
+							// see if our voter is hidden or default
+							String attrHidden = node.getAttribute("hidden");
+							if (attrHidden.toLowerCase().equals("true")) { System.err.println("hidden is true"); voter.setHidden(true); }
+							String attrDefault = node.getAttribute("default");
+							if (attrDefault.toLowerCase().equals("true")) { System.err.println("default is true"); voter.setDefault(true); }
+
+							// add the voter to our master list
+							voters.add(voter);
+						} catch (Exception ee) {
+							System.err.println("(E) MatchManager - Failed to locate voter class " + node.getAttribute("id"));
+						}
+					}
+				}
 			}
-		    catch(Exception e) { System.out.println("(E)MatcherManager - Failed to locate merger class "+mergerMatcher.group(1)); }
+
+			// get all merger nodes
+			{
+				NodeList nodes = root.getElementsByTagName("merger");
+				if (nodes != null && nodes.getLength() > 0) {
+					for (int i = 0; i < nodes.getLength(); i++) {
+						// get the element and its attribute
+						Element node = (Element)nodes.item(i);
+
+						try {
+							// create a new voter from this class id
+							Class mergerClass = Class.forName(node.getAttribute("id"));
+							MatchMerger merger = (MatchMerger)mergerClass.newInstance();
+	
+							// add the voter to our master list
+							mergers.add(merger);
+						} catch (Exception ee) {
+							System.err.println("(E) MatchManager - Failed to locate merger class " + node.getAttribute("id"));
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("(E) MatcherManager - matchers.xml has failed to load! " + e.getMessage());
+		}
 	}
 
 	/** Returns the list of match voters */
-	static public ArrayList<MatchVoter> getVoters()
-		{ return voters; }
+	static public ArrayList<MatchVoter> getVoters() {
+		return voters;
+	}
 
 	/** Returns the list of default match voters */
-	static public ArrayList<MatchVoter> getDefaultVoters()
-	{
+	static public ArrayList<MatchVoter> getDefaultVoters() {
 		ArrayList<MatchVoter> defaultVoters = new ArrayList<MatchVoter>();
-		for(MatchVoter voter : voters)
-			if(voter.isDefault()) defaultVoters.add(voter);
+		for(MatchVoter voter : voters) {
+			if (voter.isDefault()) {
+				defaultVoters.add(voter);
+			}
+		}
 		return defaultVoters;
 	}
 
 	/** Returns the list of visible match voters */
-	static public ArrayList<MatchVoter> getVisibleVoters()
-	{
+	static public ArrayList<MatchVoter> getVisibleVoters() {
 		ArrayList<MatchVoter> visibleVoters = new ArrayList<MatchVoter>();
-		for(MatchVoter voter : voters)
-			if(!voter.isHidden()) visibleVoters.add(voter);
+		for (MatchVoter voter : voters) {
+			if (!voter.isHidden()) {
+				visibleVoters.add(voter);
+			}
+		}
 		return visibleVoters;
 	}
 	
 	/** Returns the list of match mergers */
-	static public ArrayList<MatchMerger> getMergers()
-		{ return mergers; }
+	static public ArrayList<MatchMerger> getMergers() {
+		return mergers;
+	}
 
 	/** Run the matchers to calculate match scores */
-	static public MatchScores getScores(FilteredSchemaInfo schemaInfo1, FilteredSchemaInfo schemaInfo2, ArrayList<MatchVoter> voters, MatchMerger merger)
-		{ return getScores(schemaInfo1,schemaInfo2,voters,merger,null); }
+	static public MatchScores getScores(FilteredSchemaInfo schemaInfo1, FilteredSchemaInfo schemaInfo2, ArrayList<MatchVoter> voters, MatchMerger merger) {
+		return getScores(schemaInfo1, schemaInfo2, voters, merger, null);
+	}
 	
 	/** Run the matchers to calculate match scores */
-	static public MatchScores getScores(FilteredSchemaInfo schema1, FilteredSchemaInfo schema2, ArrayList<MatchVoter> voters, MatchMerger merger, MatchTypeMappings typeMappings)
-	{
+	static public MatchScores getScores(FilteredSchemaInfo schema1, FilteredSchemaInfo schema2, ArrayList<MatchVoter> voters, MatchMerger merger, MatchTypeMappings typeMappings) {
 		merger.initialize(schema1, schema2, typeMappings);
-		for(MatchVoter voter : voters)
-		{
+		for (MatchVoter voter : voters) {
 			voter.initialize(schema1, schema2, typeMappings);
 			merger.addVoterScores(voter.match());
 		}
