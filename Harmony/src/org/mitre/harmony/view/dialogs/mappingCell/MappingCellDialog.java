ae8114f2938b47c47613b632034ccecf3836cf9b
kzheng <kzheng@mitre.org>
2010-07-08 14:04:45 +0000

    Add handling function feature.

--- Harmony/src/org/mitre/harmony/view/dialogs/mappingCell/MappingCellDialog.java
+++ Harmony/src/org/mitre/harmony/view/dialogs/mappingCell/MappingCellDialog.java
@@ -52,13 +52,13 @@ public class MappingCellDialog extends JDialog implements MouseListener, MouseMo
 	/** Stores the list of mapping cells to which this dialog pertains */
 	private ArrayList<MappingCell> mappingCells;
 	
-	private JRadioButton confidenceRadioButton;
-	private JRadioButton functionRadioButton;
-	private ButtonGroup bgroup;
+	//private JRadioButton confidenceRadioButton;
+	//private JRadioButton functionRadioButton;
+	//private ButtonGroup bgroup;
 	
 	public ButtonPane btPane;
 	
-	private boolean usingFunction = false;
+	private boolean usingFunction = true;
 	
 	/** Private class for defining the button pane */
 	private class ButtonPane extends AbstractButtonPane
@@ -72,58 +72,78 @@ public class MappingCellDialog extends JDialog implements MouseListener, MouseMo
 		{
 			if(label.equals("OK"))
 			{
-				//For confience setting
-				if(!usingFunction){
 					// Throw out mapping cells if they have been rejected
 					if(confidencePane.isRejected()){
 						harmonyModel.getMappingManager().deleteMappingCells(mappingCells);
 					}
-					else
-					{			
-						// Adjust the mapping cells to reflect changes
-						ArrayList<MappingCell> newMappingCells = annotationPane.getMappingCells();
-						harmonyModel.getMappingManager().setMappingCells(newMappingCells);
-						
-						// If the mapping cells were accepted, validate as needed
-						if(confidencePane.isAccepted()){
-							harmonyModel.getMappingManager().validateMappingCells(newMappingCells);
+					else{
+						//For none functional mapping cells
+						if(!usingFunction ||functionPane.getFunctionName()==""||functionPane.getFunctionName()==null){
+							// Throw out mapping cells if they have been rejected
+							if(confidencePane.isRejected()){
+								harmonyModel.getMappingManager().deleteMappingCells(mappingCells);
+							}
+							else
+							{			
+								// Adjust the mapping cells to reflect changes
+								ArrayList<MappingCell> newMappingCells = annotationPane.getMappingCells();
+								harmonyModel.getMappingManager().setMappingCells(newMappingCells);
+								
+								// If the mapping cells were accepted, validate as needed
+								if(confidencePane.isAccepted()){
+									harmonyModel.getMappingManager().validateMappingCells(newMappingCells);
+								}
+							}
+						}
+						else{
+							/*
+							if(functionPane.getFunctionName()==null||functionPane.getFunctionName()==""){
+								//No function selected
+								System.out.println("No function selected.");									
+							}
+							else{
+							*/
+							//for existing function mapping cell
+							if(mappingCells.size()==1 && !((((MappingCell)mappingCells.get(0)).getFunctionID().toString()).equals("450"))
+									&&functionPane.getInputs().length==0&&!functionPane.getFunctionName().equals("<None>")){
+								//no change
+								System.out.println("functionName=" +functionPane.getFunctionName());
+							}	
+							else{ 
+								//new mapping cell
+								//using the function matching
+
+								MappingManager manager = harmonyModel.getMappingManager();
+					
+								//get the mapping ID:
+								Integer mappingId = mappingCells.get(0).getMappingId();			
+								//System.out.println("mappingID=" + mappingId);
+								String author = System.getProperty("user.name");
+								Date date = Calendar.getInstance().getTime();
+								//String function = IdentityFunction.class.getCanonicalName();
+								String function = functionPane.getFunctionName();
+								System.out.println("FuncName=" + function);
+								Integer[] inputs = functionPane.getInputs();
+								Integer output = functionPane.getOutput();
+								Integer functionID = functionPane.getFunctionId();
+								
+								System.out.println("output=" + output);
+								
+								//delete mapping cells
+								harmonyModel.getMappingManager().deleteMappingCells(mappingCells);
+								
+								//create a function mappingcell
+								System.out.println("function=" +function);
+								
+								if(!function.equals("<None>")){
+									MappingCell mappingCell = MappingCell.createFunctionMappingCell(null, mappingId, inputs, output, functionID, author, date, null);    	
+									
+									//MappingCell mappingCell = MappingCell.createValidatedMappingCell(id, mappingID, new Integer[]{leftID}, rightID, author, date, function, null);
+									manager.getMapping(mappingId).setMappingCells(Arrays.asList(new MappingCell[]{mappingCell}));	
+								}
+							}
 						}
 					}
-				}
-				else{
-					if(functionPane.getFunctionName()==null||functionPane.getFunctionName()==""){
-						//No function selected
-						System.out.println("No function selected.");									
-					}
-					else{
-						//using the function matching
-						//generate a new function mapping cell
-						MappingManager manager = harmonyModel.getMappingManager();
-			
-						//get the mapping ID:
-						Integer mappingId = mappingCells.get(0).getMappingId();			
-						//System.out.println("mappingID=" + mappingId);
-						String author = System.getProperty("user.name");
-						Date date = Calendar.getInstance().getTime();
-						//String function = IdentityFunction.class.getCanonicalName();
-						String function = functionPane.getFunctionName();
-						System.out.println("FuncName=" + function);
-						Integer[] inputs = functionPane.getInputs();
-						Integer output = functionPane.getOutput();
-						Integer functionID = functionPane.getFunctionId();
-						
-						System.out.println("output=" + output);
-						
-						//delete mapping cells
-						harmonyModel.getMappingManager().deleteMappingCells(mappingCells);
-						
-						//create a function mappingcell
-						MappingCell mappingCell = MappingCell.createFunctionMappingCell(null, mappingId, inputs, output, functionID, author, date, null);    	
-						
-						//MappingCell mappingCell = MappingCell.createValidatedMappingCell(id, mappingID, new Integer[]{leftID}, rightID, author, date, function, null);
-						manager.getMapping(mappingId).setMappingCells(Arrays.asList(new MappingCell[]{mappingCell}));	
-					}
-				}
 			}
 				
 			
@@ -152,6 +172,7 @@ public class MappingCellDialog extends JDialog implements MouseListener, MouseMo
 		matchingPanel.setLayout(new BorderLayout());
 			
 		//For radio button
+		/*
 		confidenceRadioButton = new JRadioButton("Confidence", true);
 		confidenceRadioButton.setFont(new Font("Arial", Font.PLAIN, 11));
 		confidenceRadioButton.setActionCommand("confidence");
@@ -165,33 +186,37 @@ public class MappingCellDialog extends JDialog implements MouseListener, MouseMo
 		bgroup.add(functionRadioButton);
 		
 		confidenceRadioButton.addActionListener(this);
+	
 		functionRadioButton.addActionListener(this);
+		*/
 		
 		JPanel radioPanel = new JPanel();
 		//radioPanel.setLayout(new GridLayout(1, 2));
+		/*
 		JLabel setMatchingLabel = new JLabel("Set matching type:");
 		radioPanel.add(setMatchingLabel);
 		radioPanel.add(confidenceRadioButton);
 		radioPanel.add(functionRadioButton);
-		
+		*/
 		JPanel centerPanel = new JPanel();
 		centerPanel.setLayout(new BorderLayout());	
-		
-		centerPanel.add(confidencePane = new MappingCellConfidencePane(mappingCells),BorderLayout.NORTH);	
+
 		centerPanel.add(functionPane = new MappingCellFunctionPane(mappingCells, harmonyModel, this), BorderLayout.SOUTH);
+		centerPanel.add(confidencePane = new MappingCellConfidencePane(mappingCells, functionPane), BorderLayout.NORTH);	
+
 		//centerPanel.setBorder(BorderFactory.createTitledBorder("Set Matching"));
-		functionPane.setDisable(true);
+		functionPane.setDisable(false);
 		confidencePane.setDisable(false);
 		
-		//Determine if it is a function mapping cell
+		//Determine if it is a function mapping cell for edition
 		if(mappingCells.size()==1){
 			for(MappingCell mappingCell : mappingCells){
 				if(!(mappingCell.getFunctionID().toString()).equals("450")){
 					//It is a function mapping cell
-					confidenceRadioButton.setSelected(false);
-					functionRadioButton.setSelected(true);
+					//confidenceRadioButton.setSelected(false);
+					//functionRadioButton.setSelected(true);
 					functionPane.setDisable(false);
-					confidencePane.setDisable(true);
+					//confidencePane.setDisable(true);
 					usingFunction = true;
 				}
 			}
@@ -205,8 +230,7 @@ public class MappingCellDialog extends JDialog implements MouseListener, MouseMo
 
 		btPane = new ButtonPane();
 		pane.add(btPane,BorderLayout.SOUTH);
-
-		
+	
 		// Set up ability to escape out of dialog
 		Action escape = new AbstractAction() { public void actionPerformed(ActionEvent arg0) { dispose(); } };
 		pane.getInputMap().put(KeyStroke.getKeyStroke((char)KeyEvent.VK_ESCAPE),"escape");
@@ -274,7 +298,7 @@ public class MappingCellDialog extends JDialog implements MouseListener, MouseMo
 	
 	//set functionRadioButton disabled.
 	public void enableFunctionRadioButton(boolean trueorfalse){
-		functionRadioButton.setEnabled(trueorfalse);
+		//functionRadioButton.setEnabled(trueorfalse);
 	}
 	
 }
