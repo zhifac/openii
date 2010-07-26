fa3cb5a5b1dc88fa1c2335124fa66f22cb1c272e
hudsonwolf <hudsonwolf@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-07-26 23:32:54 +0000

    Add "Base Schema" field to M3 Schema view

--- trunk/OpenII/src/org/mitre/openii/editors/schemas/M3SchemaView.java
+++ trunk/OpenII/src/org/mitre/openii/editors/schemas/M3SchemaView.java
@@ -3,6 +3,7 @@ package org.mitre.openii.editors.schemas;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -25,6 +26,9 @@ import org.mitre.schemastore.model.schemaInfo.SchemaInfo;
 /** Constructs the Schema View */
 public class M3SchemaView extends OpenIIEditor
 {	
+	/** Generate a listing of base schema references */
+	private HashMap<Integer,String> baseSchemas = null;
+	
 	/** Returns the list of schema elements for the specified element type */
 	private ArrayList<SchemaElement> getElements(SchemaInfo schemaInfo, Class<?> type)
 	{
@@ -41,6 +45,32 @@ public class M3SchemaView extends OpenIIEditor
 		return elements;
 	}
 	
+	/** Generates field information */
+	private String[] getFields(String... labels)
+	{
+		ArrayList<String> fields = new ArrayList<String>();
+		fields.add("ID");
+		fields.add("Name");
+		if(baseSchemas!=null) fields.add("Base Schema");
+		for(String label : labels) fields.add(label);
+		return fields.toArray(new String[0]);
+	}
+
+	/** Generate a data row */
+	private Object[] getDataRow(SchemaElement element, Object... items)
+	{
+		ArrayList<Object> data = new ArrayList<Object>();
+		data.add(element.getId());
+		data.add(element.getName());
+		if(baseSchemas!=null)
+		{
+			String schemaName = baseSchemas.get(element.getBase());
+			data.add(schemaName==null ? "" : schemaName + " (" + element.getBase() + ")");
+		}
+		for(Object item : items) data.add(item);
+		return data.toArray();
+	}
+	
 	/** Generate the schema element panes */
 	private void generateElementPanes(ExpandBar bar)
 	{
@@ -48,71 +78,61 @@ public class M3SchemaView extends OpenIIEditor
 		SchemaInfo schemaInfo = OpenIIManager.getSchemaInfo(getElementID());
 
 		// Generate the entities table
-		String[] fields = new String[]{"ID","Name","Description"};
+		String[] fields = getFields("Description");
 		ArrayList<Object[]> rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,Entity.class))
 		{
 			Entity entity = (Entity)element;
-			Integer id = entity.getId();
-			String name = entity.getName();
 			String description = entity.getDescription();
-			rows.add(new Object[]{id,name,description});
+			rows.add(getDataRow(entity,description));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Entities", fields, rows);
 
 		// Generate the attributes table
-		fields = new String[]{"ID","Name","Entity","Domain","Min","Max","IsKey","Description"};
+		fields = getFields("Entity","Domain","Min","Max","IsKey","Description");
 		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,Attribute.class))
 		{
 			Attribute attribute = (Attribute)element;
-			Integer id = attribute.getId();
-			String name = attribute.getName();
 			String entity = schemaInfo.getElement(attribute.getEntityID()).getName() + " (" + attribute.getEntityID() + ")";
 			String domain = schemaInfo.getElement(attribute.getDomainID()).getName() + " (" + attribute.getDomainID() + ")";
 			Integer min = attribute.getMin();
 			Integer max = attribute.getMax();
 			Boolean isKey = attribute.isKey();
 			String description = attribute.getDescription();
-			rows.add(new Object[]{id,name,entity,domain,min,max,isKey,description});
+			rows.add(getDataRow(attribute,entity,domain,min,max,isKey,description));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Attributes", fields, rows);
 
 		// Generate the domains table
-		fields = new String[]{"ID","Name","Description"};
+		fields = getFields("Description");
 		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,Domain.class))
 		{
 			Domain domain = (Domain)element;
-			Integer id = domain.getId();
-			String name = domain.getName();
 			String description = domain.getDescription();
-			rows.add(new Object[]{id,name,description});
+			rows.add(getDataRow(domain,description));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Domain", fields, rows);
 
 		// Generate the domain values table
-		fields = new String[]{"ID","Name","Domain","Description"};
+		fields = getFields("Domain","Description");
 		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,DomainValue.class))
 		{
 			DomainValue domainValue = (DomainValue)element;
-			Integer id = domainValue.getId();
-			String name = domainValue.getName();
 			String domain = schemaInfo.getElement(domainValue.getDomainID()).getName() + " (" + domainValue.getDomainID() + ")";
 			String description = domainValue.getDescription();
-			rows.add(new Object[]{id,name,domain,description});
+			rows.add(getDataRow(domainValue,domain,description));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Domain Values", fields, rows);
 
 		// Generate the relationships table
-		fields = new String[]{"ID","Name","Left Element","Left Min","Left Max","Right Element","Right Min","Right Max","Description"};
+		fields = getFields("Left Element","Left Min","Left Max","Right Element","Right Min","Right Max","Description");
 		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,Relationship.class))
 		{
 			Relationship relationship = (Relationship)element;
-			Integer id = relationship.getId();
-			String name = relationship.getName();
 			String left = schemaInfo.getElement(relationship.getLeftID()).getName() + " (" + relationship.getLeftID() + ")";
 			Integer leftMin = relationship.getLeftMin();
 			Integer leftMax = relationship.getLeftMax();
@@ -120,52 +140,46 @@ public class M3SchemaView extends OpenIIEditor
 			Integer rightMin = relationship.getRightMin();
 			Integer rightMax = relationship.getRightMax();
 			String description = relationship.getDescription();
-			rows.add(new Object[]{id,name,left,leftMin,leftMax,right,rightMin,rightMax,description});
+			rows.add(getDataRow(relationship,left,leftMin,leftMax,right,rightMin,rightMax,description));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Relationships", fields, rows);
 
 		// Generate the containments table
-		fields = new String[]{"ID","Name","Parent Element","Child Element","Min","Max","Description"};
+		fields = getFields("Parent Element","Child Element","Min","Max","Description");
 		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,Containment.class))
 		{
 			Containment containment = (Containment)element;
-			Integer id = containment.getId();
-			String name = containment.getName();
 			String parentElement = containment.getParentID()==null ? "" : schemaInfo.getElement(containment.getParentID()).getName() + " (" + containment.getParentID() + ")";
 			String childElement = schemaInfo.getElement(containment.getChildID()).getName() + " (" + containment.getChildID() + ")";
 			Integer min = containment.getMin();
 			Integer max = containment.getMax();
 			String description = containment.getDescription();
-			rows.add(new Object[]{id,name,parentElement,childElement,min,max,description});
+			rows.add(getDataRow(containment,parentElement,childElement,min,max,description));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Containments", fields, rows);
 
 		// Generate the subtypes table
-		fields = new String[]{"ID","Name","Parent Element","Child Element","Description"};
+		fields = getFields("Parent Element","Child Element","Description");
 		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,Subtype.class))
 		{
 			Subtype subtype = (Subtype)element;
-			Integer id = subtype.getId();
-			String name = subtype.getName();
 			String parentElement = schemaInfo.getElement(subtype.getParentID()).getName() + " (" + subtype.getParentID() + ")";
 			String childElement = schemaInfo.getElement(subtype.getChildID()).getName() + " (" + subtype.getChildID() + ")";
 			String description = subtype.getDescription();
-			rows.add(new Object[]{id,name,parentElement,childElement,description});
+			rows.add(getDataRow(subtype,parentElement,childElement,description));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Subtypes", fields, rows);
 
 		// Generate the aliases table
-		fields = new String[]{"ID","Name","Aliased Element"};
+		fields = getFields("Aliased Element");
 		rows = new ArrayList<Object[]>();
 		for(SchemaElement element : getElements(schemaInfo,Alias.class))
 		{
 			Alias alias = (Alias)element;
-			Integer id = alias.getId();
-			String name = alias.getName();
 			String aliasedElement = schemaInfo.getElement(alias.getElementID()).getName() + " (" + alias.getElementID() + ")";
-			rows.add(new Object[]{id,name,aliasedElement});
+			rows.add(getDataRow(alias,aliasedElement));
 		}
 		ExpandBarWidgets.createTablePane(bar, "Aliases", fields, rows);
 	}
@@ -173,13 +187,23 @@ public class M3SchemaView extends OpenIIEditor
 	/** Displays the Schema View */
 	public void createPartControl(Composite parent)
 	{		
+		Schema schema = OpenIIManager.getSchema(getElementID());
+
+		// Generate references to parent schemas
+		ArrayList<Integer> ancestorIDs = OpenIIManager.getAncestorSchemas(schema.getId());
+		if(ancestorIDs.size()>0)
+		{
+			baseSchemas = new HashMap<Integer,String>();
+			for(Integer ancestorID : ancestorIDs)
+				baseSchemas.put(ancestorID, OpenIIManager.getSchema(ancestorID).getName());
+		}
+		
 		// Create the expand bar
 		ExpandBar bar = new ExpandBar(parent, SWT.V_SCROLL);
 		bar.setSpacing(8);
 
 		// Creates the properties pane
 		ArrayList<String> attributes = new ArrayList<String>();
-		Schema schema = OpenIIManager.getSchema(getElementID());
 		attributes.add("Name:"+schema.getName()+" ("+schema.getId()+")");
 		attributes.add("Author:"+schema.getAuthor());
 		attributes.add("Source:"+schema.getSource());
