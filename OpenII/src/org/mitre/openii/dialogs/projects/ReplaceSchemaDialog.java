e50542c5fb9a4d181bd74b451a3000cbd46a0e52
hudsonwolf <hudsonwolf@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-07-28 19:40:21 +0000

    Install the ReplaceSchema dialog

--- OpenII/src/org/mitre/openii/dialogs/projects/ReplaceSchemaDialog.java
+++ OpenII/src/org/mitre/openii/dialogs/projects/ReplaceSchemaDialog.java
@@ -1,5 +1,7 @@
 package org.mitre.openii.dialogs.projects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 import org.eclipse.jface.dialogs.Dialog;
@@ -12,25 +14,34 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.mitre.openii.application.OpenIIActivator;
 import org.mitre.openii.model.OpenIIManager;
+import org.mitre.openii.model.RepositoryManager;
 import org.mitre.openii.views.manager.SchemaInProject;
 import org.mitre.openii.widgets.BasicWidgets;
+import org.mitre.schemastore.model.Mapping;
+import org.mitre.schemastore.model.Project;
+import org.mitre.schemastore.model.ProjectSchema;
 import org.mitre.schemastore.model.Schema;
+import org.mitre.schemastore.porters.PorterManager;
+import org.mitre.schemastore.porters.mappingExporters.M3MappingExporter;
+import org.mitre.schemastore.porters.mappingImporters.M3MappingImporter;
+import org.w3c.dom.Document;
 
 /** Constructs the Replace Schema Dialog */
 public class ReplaceSchemaDialog extends Dialog implements ISelectionChangedListener
 {
 	/** Stores the project schema being replaced */
-	private SchemaInProject schema = null;
+	private SchemaInProject oldSchema = null;
 
 	/** Stores the list of schemas which can replace the project schema */
-	private ComboViewer replacementList = null;
+	private ComboViewer schemaList = null;
 
 	/** Constructs the dialog */
-	public ReplaceSchemaDialog(Shell shell, SchemaInProject schema)
-		{ super(shell); this.schema = schema; }
+	public ReplaceSchemaDialog(Shell shell, SchemaInProject oldSchema)
+		{ super(shell); this.oldSchema = oldSchema; }
 
 	/** Configures the dialog shell */
 	protected void configureShell(Shell shell)
@@ -45,7 +56,7 @@ public class ReplaceSchemaDialog extends Dialog implements ISelectionChangedList
 	{
 		// Identify possible replacement schemas
 		HashSet<Schema> schemas = new HashSet<Schema>(OpenIIManager.getSchemas());
-		for(Integer schemaID : OpenIIManager.getProject(schema.getProjectID()).getSchemaIDs())
+		for(Integer schemaID : OpenIIManager.getProject(oldSchema.getProjectID()).getSchemaIDs())
 			for(Schema schema : schemas)
 				if(schema.getId().equals(schemaID))
 					{ schemas.remove(schema); break; }
@@ -61,7 +72,7 @@ public class ReplaceSchemaDialog extends Dialog implements ISelectionChangedList
 		layout = new GridLayout(2,false);
 		layout.marginWidth = 8;
 		schemaPane.setLayout(layout);
-		replacementList = BasicWidgets.createComboField(schemaPane, "Replacement", schemas.toArray(), this);
+		schemaList = BasicWidgets.createComboField(schemaPane, "Replacement", schemas.toArray(), this);
 
 		// Return the generated pane
 		return pane;
@@ -77,9 +88,84 @@ public class ReplaceSchemaDialog extends Dialog implements ISelectionChangedList
 
 	/** Returns the selected schema for the specified list */
 	private Schema getSchema()
-		{ return (Schema)((StructuredSelection)replacementList.getSelection()).getFirstElement(); }
+		{ return (Schema)((StructuredSelection)schemaList.getSelection()).getFirstElement(); }
 	
 	/** Handles changes to the selected schemas */
 	public void selectionChanged(SelectionChangedEvent e)
 		{ getButton(IDialogConstants.OK_ID).setEnabled(getSchema()!=null); }
+	
+	/** Handles the actual replacement of the schema */
+	protected void okPressed()
+	{
+		String errorMessage = null;
+
+		// Get project and schema information
+		Project project = OpenIIManager.getProject(oldSchema.getProjectID());
+		Schema schema = getSchema();
+		
+		// Add new schema to project
+		ArrayList<ProjectSchema> schemas = new ArrayList<ProjectSchema>(Arrays.asList(project.getSchemas()));
+		schemas.add(new ProjectSchema(schema.getId(),schema.getName(),null));
+		project.setSchemas(schemas.toArray(new ProjectSchema[0]));
+		if(!OpenIIManager.updateProject(project))
+			errorMessage = "Unable to add " + schema.getName() + " to the project.'";
+		
+		// Add new mappings to project
+		if(errorMessage==null)
+		{
+			// Retrieve the M3 Mapping importer and exporter
+			PorterManager manager = new PorterManager(RepositoryManager.getClient());
+			M3MappingExporter exporter = (M3MappingExporter) manager.getPorter(M3MappingExporter.class);
+			M3MappingImporter importer = (M3MappingImporter) manager.getPorter(M3MappingImporter.class);
+
+			// Transfer all mappings
+			for(Mapping mapping : OpenIIManager.getMappings(project.getId()))
+			{
+				boolean isSource = mapping.getSourceId().equals(oldSchema.getSchema().getId());
+				boolean isTarget = mapping.getTargetId().equals(oldSchema.getSchema().getId());
+				if(isSource || isTarget)
+				{
+					Integer sourceID = isSource ? schema.getId() : mapping.getSourceId();
+					Integer targetID = isTarget ? schema.getId() : mapping.getTargetId();
+					try {
+						// Transfer the mapping to the new schema
+						Document document = exporter.generateXMLDocument(mapping, OpenIIManager.getMappingCells(mapping.getId()));
+						importer.initialize(document);
+						if(importer.importMapping(project, sourceID, targetID)==null)
+							throw new Exception();
+					}
+					catch(Exception e)
+					{
+						String sourceName = OpenIIManager.getSchema(sourceID).getName();
+						String targetName = OpenIIManager.getSchema(targetID).getName();
+						errorMessage = "Unable to add mapping between " + sourceName + " and " + targetName;
+					}
+				}
+			}
+
+			// Remove old mappings (or rollback new mappings in case of failure)
+			Integer removalID = errorMessage==null ? oldSchema.getSchema().getId() : schema.getId();
+			for(Mapping mapping : OpenIIManager.getMappings(project.getId()))
+				if(mapping.getSourceId().equals(removalID) || mapping.getTargetId().equals(removalID))
+					OpenIIManager.deleteMapping(mapping.getId());
+
+			// Removes reference to old schema (or rollback new schema in case of failure)
+			for(ProjectSchema currSchema : schemas)
+				if(currSchema.getId().equals(removalID))
+					{ schemas.remove(currSchema); break; }
+			project.setSchemas(schemas.toArray(new ProjectSchema[0]));
+			OpenIIManager.updateProject(project);
+		}		
+		
+		// Display error message if needed
+		if(errorMessage != null)
+		{
+			MessageBox messageBox = new MessageBox(getShell(), SWT.ERROR);
+			messageBox.setText("Error Replacing Schema");
+			messageBox.setMessage(errorMessage);
+			messageBox.open();
+			return;
+		}
+		else getShell().dispose();
+	}
 }
