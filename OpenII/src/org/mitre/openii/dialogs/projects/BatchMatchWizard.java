ed8a8b7b04d4337907405bf728539c6415bfd33a
plockaby <plockaby@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-08-09 14:20:14 +0000

    Removed "@Override" declarations that were causing compile errors. (There is still another compile error due to a bad method call but someone else needs to fix that. Don't know where the problem is.)_

--- OpenII/src/org/mitre/openii/dialogs/projects/BatchMatchWizard.java
+++ OpenII/src/org/mitre/openii/dialogs/projects/BatchMatchWizard.java
@@ -19,127 +19,106 @@ public class BatchMatchWizard implements IWizard {
 		// TODO
 	}
 
-	@Override
 	public void addPages() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public boolean canFinish() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public void createPageControls(Composite arg0) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public IWizardContainer getContainer() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Image getDefaultPageImage() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public IDialogSettings getDialogSettings() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public IWizardPage getNextPage(IWizardPage arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public IWizardPage getPage(String arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public int getPageCount() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public IWizardPage[] getPages() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public IWizardPage getPreviousPage(IWizardPage arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public IWizardPage getStartingPage() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public RGB getTitleBarColor() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String getWindowTitle() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public boolean isHelpAvailable() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean needsPreviousAndNextButtons() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean needsProgressMonitor() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean performCancel() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean performFinish() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public void setContainer(IWizardContainer arg0) {
 		// TODO Auto-generated method stub
 
