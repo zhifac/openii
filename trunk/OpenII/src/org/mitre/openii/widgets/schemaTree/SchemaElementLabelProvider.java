d67206ed10f0e3aa3906cde692cff8069453579c
hudsonwolf <hudsonwolf@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-09-30 16:02:18 +0000

    Lighten the color of  the extra information which can be viewed about schema elements

--- trunk/OpenII/src/org/mitre/openii/widgets/schemaTree/SchemaElementLabelProvider.java
+++ trunk/OpenII/src/org/mitre/openii/widgets/schemaTree/SchemaElementLabelProvider.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.eclipse.jface.viewers.StyledCellLabelProvider;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
@@ -26,6 +27,8 @@ public class SchemaElementLabelProvider extends StyledCellLabelProvider
 	private static final Color YELLOW = Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW); 
 	private static final Color ORANGE = new Color(Display.getCurrent(), 255, 200, 145); 
 	private static final Color WHITE = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE); 
+	private static final Color GRAY = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GRAY); 
+	private static final Color BLACK = Display.getCurrent().getSystemColor(SWT.COLOR_BLACK); 
 	
 	/** Stores a reference to the schema view */
 	private SchemaTree schemaView = null;
@@ -55,12 +58,20 @@ public class SchemaElementLabelProvider extends StyledCellLabelProvider
 	{
 		String text = "";
 		if(object instanceof SchemaElement)
+			text = schemaView.getSchema().getDisplayName(((SchemaElement)object).getId());
+		else text = object.toString();
+		return text;
+	}
+
+	/** Returns the specified element's associated info */
+	public String getAssociatedInfo(Object object)
+	{
+		String text = "";
+		if(object instanceof SchemaElement)
 		{
+			// Get the schema element
 			HierarchicalSchemaInfo schema = schemaView.getSchema();
-			
-			// Display the element name
 			SchemaElement element = (SchemaElement)object;
-			text = schema.getDisplayName(element.getId());
 			
 			// Displays the type of each element if requested
 			if(schemaView.showTypes())
@@ -81,10 +92,9 @@ public class SchemaElementLabelProvider extends StyledCellLabelProvider
 				}
 			}
 		}
-		else text = object.toString();
 		return text;
 	}
-
+	
 	/** Displays the tool tip for the specified element */
 	public String getToolTipText(Object element)
 	{
@@ -96,24 +106,30 @@ public class SchemaElementLabelProvider extends StyledCellLabelProvider
 	/** Updates the viewer cell */
 	public void update(ViewerCell cell)
 	{
-		// Set up the cell text and image
+		// Display the cell image
 		Object element = cell.getElement();
 		cell.setImage(getImage(element));
-		cell.setText(getText(element));
+		
+		// Display the cell text
+		String text = getText(element);
+		String associatedInfo = getAssociatedInfo(element);
+		cell.setText(text + associatedInfo);
 
-		// Highlight cells which match the search results
+		// Determine the highlight color
+		Color highlightColor = WHITE;
 		if(element instanceof SchemaElement)
 		{
-			// Get the search result
 			SchemaElement schemaElement = (SchemaElement)element;
 			SchemaSearchResult searchResult = schemaView.getSearchResult(schemaElement.getId());
-
-			// Assign the background color
-			Color color = WHITE;
 			if(searchResult!=null)
-				{ color = searchResult.nameMatched() ? YELLOW : searchResult.descriptionMatched() ? ORANGE : WHITE; }
-			cell.setBackground(color);
+				{ highlightColor = searchResult.nameMatched() ? YELLOW : searchResult.descriptionMatched() ? ORANGE : WHITE; }
 		}
+		
+		// Assign the background color
+		StyleRange styleRanges[] = new StyleRange[2];
+		styleRanges[0] = new StyleRange(0,text.length(),BLACK,highlightColor);
+		styleRanges[1] = new StyleRange(text.length(),associatedInfo.length(),GRAY,WHITE);
+		cell.setStyleRanges(styleRanges);
 		
 		super.update(cell);
 	}
