9fa3630371aed0701c262b95b94fb87605403c28
hudsonwolf <hudsonwolf@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2010-12-14 19:15:40 +0000

    Fix bug in displaying project mappings when imported

--- Harmony/src/org/mitre/harmony/view/dialogs/importers/ImportProjectDialog.java
+++ Harmony/src/org/mitre/harmony/view/dialogs/importers/ImportProjectDialog.java
@@ -9,9 +9,10 @@ import java.util.ArrayList;
 
 import org.mitre.harmony.controllers.ProjectController;
 import org.mitre.harmony.model.HarmonyModel;
-import org.mitre.harmony.model.SchemaStoreManager;
 import org.mitre.harmony.model.HarmonyModel.InstantiationType;
+import org.mitre.harmony.model.SchemaStoreManager;
 import org.mitre.schemastore.model.Project;
+import org.mitre.schemastore.porters.Importer;
 import org.mitre.schemastore.porters.PorterType;
 import org.mitre.schemastore.porters.projectImporters.ProjectImporter;
 
@@ -34,18 +35,27 @@ public class ImportProjectDialog extends AbstractImportDialog
 		return usedNames;
 	}
 	
+	/** Imports the specified schema locally */
+	private Integer importItemLocally(Importer importer, String name, String author, String description, URI uri) throws Exception
+	{
+		ProjectImporter projectImporter = (ProjectImporter)selectionList.getSelectedItem();
+		projectImporter.initialize(uri);
+		return projectImporter.importProject(name, author, description);
+	}
+	
 	/** Imports the currently specified project */
 	protected void importItem(String name, String author, String description, URI uri) throws Exception
 	{
 		Integer projectID = null;
 		
 		// Import the project
-		ProjectImporter importer = (ProjectImporter)selectionList.getSelectedItem();
+		Importer importer = (Importer)selectionList.getSelectedItem();
 		if(harmonyModel.getInstantiationType()==InstantiationType.WEBAPP)
 			projectID = SchemaStoreManager.importData(importer, name, author, description, uri);
-		else { importer.initialize(uri); projectID = importer.importProject(name, author, description); }
+		else projectID = importItemLocally(importer, name, author, description, uri);
 
 		// Load the imported project
 		ProjectController.loadProject(harmonyModel,projectID);
+		ProjectController.selectMappings(harmonyModel);
 	}
 }
