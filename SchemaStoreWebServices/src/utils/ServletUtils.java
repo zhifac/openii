28925b5b50f4ed80d7040ed68de8f4b074d8f72a
hudsonwolf <hudsonwolf@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2011-03-26 13:53:12 +0000

    Make all of the URIs unique

--- SchemaStoreWebServices/src/utils/ServletUtils.java
+++ SchemaStoreWebServices/src/utils/ServletUtils.java
@@ -20,4 +20,8 @@ public class ServletUtils
 	/** Retrieve the URL for displaying a supplier and what they are supplying */
 	static public String getSupplierURL(HttpServletRequest request, int id)
 		{ return getWebServiceURL(request) + "/displaySupplier.jsp?id=" + id; }
+	
+	/** Retrieve the URL for displaying supplies from a specific supplier */
+	static public String getSuppliesURL(HttpServletRequest request, int schemaID, int elementID)
+		{ return getWebServiceURL(request) + "/displaySupplier.jsp?id="+schemaID+"&elementID=" + elementID; }
 }
