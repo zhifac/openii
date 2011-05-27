6d1136efa7a31db768f27a639c6c52fe53d72a45
hudsonwolf <hudsonwolf@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2011-05-27 19:05:10 +0000

    Add in dialog to allow thesaurus to be named upon generation

--- trunk/OpenII/src/org/mitre/openii/dialogs/vocabulary/GenerateThesaurusDialog.java
+++ trunk/OpenII/src/org/mitre/openii/dialogs/vocabulary/GenerateThesaurusDialog.java
@@ -1,10 +1,11 @@
 package org.mitre.openii.dialogs.vocabulary;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.mitre.openii.dialogs.thesauri.EditThesaurusDialog;
 import org.mitre.openii.model.OpenIIManager;
 import org.mitre.openii.views.manager.VocabularyInProject;
-import org.mitre.schemastore.model.Project;
 import org.mitre.schemastore.model.Thesaurus;
 import org.mitre.schemastore.model.ThesaurusTerms;
 import org.mitre.schemastore.model.VocabularyTerms;
@@ -16,25 +17,22 @@ public class GenerateThesaurusDialog
 	{
 		// Retrieve the vocabulary to transform
 		Integer projectID = vocabularyInProject.getProjectID();
-		Project project = OpenIIManager.getProject(projectID);
 		VocabularyTerms vocabTerms = OpenIIManager.getVocabularyTerms(projectID);
+		
+		// Generate the thesaurus
+		EditThesaurusDialog dialog = new EditThesaurusDialog(shell, null);
+		dialog.open();
+		if(dialog.getReturnCode() == IDialogConstants.CANCEL_ID) return;
+		Thesaurus thesaurus = dialog.getThesaurus();
 
 		// Generate the thesaurus and thesaurus terms
-		String projectName = project.getName();
-		Thesaurus thesaurus = new Thesaurus(null, projectName, "");
-		ThesaurusTerms terms = new ThesaurusTerms(null,vocabTerms.getTerms());
-		
+		ThesaurusTerms terms = new ThesaurusTerms(null,vocabTerms.getTerms());		
+
 		// Save the thesaurus
-		boolean success = true;
-		Integer thesaurusID = OpenIIManager.addThesaurus(thesaurus);
-		if(thesaurusID!=null)
+		terms.setThesaurusId(thesaurus.getId());
+		if(!OpenIIManager.saveThesaurusTerms(terms))
 		{
-			terms.setThesaurusId(thesaurusID);
-			success = OpenIIManager.saveThesaurusTerms(terms);
-		}
-		if(!success)
-		{
-			OpenIIManager.deleteSchema(thesaurusID);		
+			OpenIIManager.deleteThesaurus(thesaurus.getId());
 			MessageDialog.openError(shell, "Generation Failure", "The thesaurus failed to be properly generated");
 		}
 	}
