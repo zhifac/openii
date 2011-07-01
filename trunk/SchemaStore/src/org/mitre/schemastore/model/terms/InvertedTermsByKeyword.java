9e85f5bc9734b09cc1370f073f47e8733612fdac
hudsonwolf <hudsonwolf@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2011-07-01 21:01:34 +0000

    Add in ability to track phrases in the inverted term list (allows quick access to understand phrase keywords available)

--- trunk/SchemaStore/src/org/mitre/schemastore/model/terms/InvertedTermsByKeyword.java
+++ trunk/SchemaStore/src/org/mitre/schemastore/model/terms/InvertedTermsByKeyword.java
@@ -5,6 +5,10 @@ package org.mitre.schemastore.model.terms;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+
+import org.mitre.schemastore.utils.Stemmer;
+import org.mitre.schemastore.utils.Tokenizer;
 
 /**
  * Class storing an inverted list of terms
@@ -12,6 +16,9 @@ import java.util.HashMap;
  */
 public class InvertedTermsByKeyword implements Serializable
 {	
+	/** Stores a lookup of all phases based on their first word */
+	private HashMap<String,ArrayList<String>> phraseList = new HashMap<String,ArrayList<String>>();
+	
 	/** Stores the inverted list of terms by keyword */
 	private HashMap<String,Terms> invertedList = new HashMap<String,Terms>();
 
@@ -22,14 +29,44 @@ public class InvertedTermsByKeyword implements Serializable
 	public InvertedTermsByKeyword(Terms terms)
 		{ for(Term term : terms.getTerms()) addTerm(term); }
 	
+	/** Generates the list of keywords under which to store the term */
+	private ArrayList<String> generateKeywords(Term term)
+	{
+		// Get the list of names under which to store the term
+		ArrayList<String> names = new ArrayList<String>();
+		names.add(term.getName());
+		for(AssociatedElement element : term.getElements())
+			names.add(element.getName());
+		
+		// Generate the list of keywords
+		ArrayList<String> keywords = new ArrayList<String>();
+		for(String name : names)
+		{
+			// Split and stem the keyword
+			ArrayList<String> words = Tokenizer.tokenize(name);
+			StringBuffer buffer = new StringBuffer();
+			for(String word : words) buffer.append(Stemmer.stem(word) + " ");
+			if(buffer.length()>0) keywords.add(buffer.substring(0, buffer.length()-1).toLowerCase());
+		}
+		return keywords;
+	}
+	
 	/** Adds a term to the inverted list */
 	public void addTerm(Term term)
 	{
-		for(AssociatedElement element : term.getElements())
+		// Cycle through each keyword
+		for(String keyword : generateKeywords(term))
 		{
-			Terms terms = invertedList.get(element.getName());
-			if(terms==null) invertedList.put(element.getName(), terms=new Terms());
+			// Add term to inverted list
+			Terms terms = invertedList.get(keyword);
+			if(terms==null) invertedList.put(keyword, terms=new Terms());
 			terms.addTerm(term);
+			
+			// Add keyword to phrase list
+			String firstWord = keyword.replaceAll(" .*", "");
+			ArrayList<String> phrases = phraseList.get(firstWord);
+			if(phrases==null) phraseList.put(firstWord, phrases=new ArrayList<String>());
+			phrases.add(keyword);
 		}
 	}
 	
@@ -44,25 +81,40 @@ public class InvertedTermsByKeyword implements Serializable
 	/** Removes a term from the inverted list */
 	public void removeTerm(Term term)
 	{
-		for(AssociatedElement element : term.getElements())
+		// Cycle through each keyword
+		for(String keyword : generateKeywords(term))
 		{
-			Terms terms = invertedList.get(element.getName());
+			// Remove the term from the inverted list
+			Terms terms = invertedList.get(keyword);
 			if(terms!=null) terms.removeTerm(term);
+		
+			// Remove the keyword from the phrase list
+			String firstWord = keyword.replaceAll(" .*", "");
+			ArrayList<String> phrases = phraseList.get(firstWord);
+			if(phrases!=null) phrases.remove(keyword);
 		}
 	}
 	
-	/** Returns the list of synonyms for the specified word */
-	public String[] getSynonyms(String keyword)
+	/** Returns the list of phrases which start with the specified word */
+	public HashSet<String> getPhrases(String word)
 	{
-		ArrayList<String> synonyms = new ArrayList<String>();
-		Terms terms = invertedList.get(keyword);
+		ArrayList<String> phrases = phraseList.get(word.toLowerCase());
+		if(phrases==null) return new HashSet<String>();
+		return new HashSet<String>(phrases);
+	}
+	
+	/** Returns the list of synonyms for the specified keyword */
+	public HashSet<String> getSynonyms(String keyword)
+	{
+		HashSet<String> synonyms = new HashSet<String>();
+		Terms terms = invertedList.get(keyword.toLowerCase());
 		if(terms!=null)
 			for(Term term : terms.getTerms())
 			{
-				synonyms.add(term.getName());
+				synonyms.addAll(Tokenizer.tokenize(term.getName()));
 				for(AssociatedElement element : term.getElements())
-					synonyms.add(element.getName());
+					synonyms.addAll(Tokenizer.tokenize(element.getName()));
 			}
-		return synonyms.toArray(new String[0]);
+		return synonyms;
 	}
 }
