62319bcdb4ff0962ae267bac23d916cede2fc93f
megreer <megreer@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2014-01-30 14:55:26 +0000

    Updated OpenII to add menu option to view the Open II version as set in the MANIFEST.MF file.  It is easily set in the Overview tab of viewing the build.properties file.

---
+++ trunk/OpenII/src/org/mitre/openii/commands/OpenIIVersion.java
@@ -0,0 +1,17 @@
+package org.mitre.openii.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.mitre.openii.views.repositories.EditRepositoryDialog;
+
+public class OpenIIVersion extends AbstractHandler{
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// customized MessageDialog with configured buttons
+		MessageDialog.openInformation(null, "OpenII Version",
+				"OpenII Version: " + Platform.getProduct().getDefiningBundle().getVersion().toString());
+		return null;
+	}
+}
