9759bb154d5269f014f6d650142d8a52d4ceef95
megreer <megreer@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2014-02-04 11:23:42 +0000

    Add a menu option to report version and include updates to Harmony Matcher and SchemaStoreClient importers.

--- trunk/OpenII/src/org/mitre/openii/commands/OpenIIVersion.java
+++ trunk/OpenII/src/org/mitre/openii/commands/OpenIIVersion.java
@@ -5,7 +5,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.mitre.openii.views.repositories.EditRepositoryDialog;
+
 
 public class OpenIIVersion extends AbstractHandler{
 	public Object execute(ExecutionEvent event) throws ExecutionException {
