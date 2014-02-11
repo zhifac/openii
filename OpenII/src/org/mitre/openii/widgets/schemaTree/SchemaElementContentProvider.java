fafed704f54a8c9bfaf448ec9e62aed980396137
megreer <megreer@ee417fe1-b928-412a-8cf9-1fee3d07e9ea>
2014-02-11 17:45:23 +0000

    Fixes bug with XSD importer and urns.
    Displays '{K}' in grey next to key attributes.  
    
    Prints attribute type instead of blank if attribute is unnamed.

--- OpenII/src/org/mitre/openii/widgets/schemaTree/SchemaElementContentProvider.java
+++ OpenII/src/org/mitre/openii/widgets/schemaTree/SchemaElementContentProvider.java
@@ -10,20 +10,20 @@ import org.mitre.schemastore.model.SchemaElement;
 import org.mitre.schemastore.model.schemaInfo.HierarchicalSchemaInfo;
 
 /** Provides Content to the Schema Tree */
-public class SchemaElementContentProvider implements ITreeContentProvider
-{
+public class SchemaElementContentProvider implements ITreeContentProvider {
 	/** Stores a reference to the schema view */
 	private SchemaTree schemaView = null;
-	
+
 	/** Constructs the content provider */
-	public SchemaElementContentProvider(SchemaTree schemaView)
-		{ this.schemaView = schemaView; }
-	
+	public SchemaElementContentProvider(SchemaTree schemaView) {
+		this.schemaView = schemaView;
+	}
+
 	/** Returns the children elements for the specified element */
-	public Object[] getChildren(Object element)
-	{
+	public Object[] getChildren(Object element) {
 		HierarchicalSchemaInfo schema = schemaView.getSchema();
-		if(element instanceof String) return new Object[]{schema.getSchema()};
+		if (element instanceof String)
+			return new Object[] { schema.getSchema() };
 		ArrayList<SchemaElementWrapper> wrappers = new ArrayList<SchemaElementWrapper>();
 		ArrayList<SchemaElement> elements;
 		if (element instanceof Schema) {
@@ -32,42 +32,56 @@ public class SchemaElementContentProvider implements ITreeContentProvider
 				wrappers.add(new SchemaElementWrapper(current));
 			}
 		} else {
-			SchemaElementWrapper wrapper = (SchemaElementWrapper)element;
-			if (!(wrapper.getPathIds().contains(schema.getType(schema, wrapper.getSchemaElement().getId())))){
-			elements = schema.getChildElements(wrapper.getSchemaElement().getId(), wrapper.getPathIds());
-			SchemaElement type = schema.getType(schema, wrapper.getSchemaElement().getId());
-			if (type instanceof Domain) {
-				type = null;
-			}
-			for (SchemaElement current : elements) {
-				SchemaElementWrapper w = wrapper.createChildWrapper(current);
-				wrappers.add(w);
-				if (type != null) {
-					w.insertTypeId(type.getId());
+			SchemaElementWrapper wrapper = (SchemaElementWrapper) element;
+			if (!(wrapper.getPathIds().contains(schema.getType(schema, wrapper
+					.getSchemaElement().getId())))) {
+				elements = schema.getChildElements(wrapper.getSchemaElement()
+						.getId(), wrapper.getPathIds());
+				SchemaElement type = schema.getType(schema, wrapper
+						.getSchemaElement().getId());
+				if (type instanceof Domain) {
+					type = null;
+				}
+				for (SchemaElement current : elements) {
+					SchemaElementWrapper w = wrapper
+							.createChildWrapper(current);
+					wrappers.add(w);
+					if (type != null) {
+						w.insertTypeId(type.getId());
 
+					}
+				}
 			}
-			}
-		}
 		}
 
 		return wrappers.toArray();
 	}
 
 	/** Return the parent element for the specified element */
-	public Object getParent(Object element)
-		{ return null; }
+	public Object getParent(Object element) {
+		return null;
+	}
 
 	/** Indicates if the specified element has any children */
-	public boolean hasChildren(Object element)
-		{ return getChildren(element).length > 0; }
+	public boolean hasChildren(Object element) {
+		return getChildren(element).length > 0;
+	}
 
 	/** Returns the list of elements to display for the specified element */
-	public Object[] getElements(Object element)
-		{ return getChildren(element); }
+	public Object[] getElements(Object element) {
+		return getChildren(element);
+	}
 
 	// Unused functions
-	public void dispose() {}
-	public void inputChanged(Viewer arg0, Object arg1, Object arg2) {}
-	public void updateChildCount(Object arg0, int arg1) {}
-	public void updateElement(Object arg0, int arg1) {}
+	public void dispose() {
+	}
+
+	public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
+	}
+
+	public void updateChildCount(Object arg0, int arg1) {
+	}
+
+	public void updateElement(Object arg0, int arg1) {
+	}
 }
